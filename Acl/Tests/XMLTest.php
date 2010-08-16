8f1de8b9f003a8c9f940a998099c4f7a1d9f96e8
Your Name <s9e.dev@gmail.com>
2010-08-16 09:44:02 +0200

    Added s9e\Acl\XMLReader, used to query an ACL stored in XML form

--- Acl/Tests/XMLTest.php
+++ Acl/Tests/XMLTest.php
@@ -3,10 +3,10 @@
 namespace s9e\Toolkit\Acl\Tests;
 
 use s9e\Toolkit\Acl\Builder;
-use s9e\Toolkit\Acl\Reader;
+use s9e\Toolkit\Acl\XMLReader;
 
 include_once __DIR__ . '/../Builder.php';
-include_once __DIR__ . '/../Reader.php';
+include_once __DIR__ . '/../XMLReader.php';
 
 class XMLTest extends \PHPUnit_Framework_TestCase
 {
@@ -19,13 +19,13 @@ class XMLTest extends \PHPUnit_Framework_TestCase
 
 		$xml = $builder->getReaderXML();
 
-		$dom = new \DOMDocument;
-		$dom->loadXML($xml);
+		$reader = new XMLReader($xml);
 
-		$x = new \DOMXPath($dom);
-
-		$this->assertSame('1', $x->evaluate('substring(/acl/space[@foo], 1 + concat("0", /acl/space[@foo]/scopes/bar/@_456), 1)'));
-
-		$this->assertSame('0', $x->evaluate('substring(/acl/space[@foo], 1 + concat("0", /acl/space[@foo]/scopes/bar/@_456) + concat("0", /acl/space[@foo]/scopes/baz/@_DENY), 1)'));
+		$this->assertFalse($reader->isAllowed('foo'));
+		$this->assertTrue($reader->isAllowed('foo', array('bar' => 456)));
+		$this->assertFalse($reader->isAllowed('foo', array('bar' => 456, 'baz' => 'DENY')));
+		$this->assertFalse($reader->isAllowed('foo', array('bar' => 123)));
+		$this->assertTrue($reader->isAllowed('foo', array('bar' => 123, 'baz' => 'xyz')));
+		$this->assertFalse($reader->isAllowed('zz', array('bar' => 123, 'baz' => 'xyz')));
 	}
 }
---
+++ Acl/XMLReader.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+* @package   s9e\toolkit
+* @copyright Copyright (c) 2010 The s9e Authors
+* @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace s9e\Toolkit\Acl;
+
+class XMLReader
+{
+	/**
+	* @var \DOMXPath
+	*/
+	protected $xpath;
+
+	/**
+	* @var string Path to the root node
+	*/
+	protected $rootPath;
+
+	public function __construct($xml, $rootPath = '/acl')
+	{
+		$dom = new \DOMDocument;
+		$dom->loadXML($xml);
+
+		$this->xpath = new \DOMXPath($dom);
+
+		$this->rootPath = rtrim($rootPath, '/');
+	}
+
+	/**
+	* @return bool
+	*/
+	public function isAllowed($perm, array $scope = array())
+	{
+		return $this->xpath->evaluate("'1'=" . self::buildXPath($this->rootPath, $perm, $scope));
+	}
+
+	/**
+	* Build the XPath needed to retrieve the character ('0' or '1') representing given perm/scope
+	*
+	* @param  string $rootPath XPath to the root node
+	* @param  string $perm
+	* @param  array  $scope
+	* @return string
+	*/
+	static public function buildXPath($rootPath, $perm, array $scope = array())
+	{
+		$nodePath = $rootPath . '/space[@' . $perm . ']';
+		$xpath    = 'substring(' . $nodePath . ',1';
+
+		foreach ($scope as $scopeDim => $scopeVal)
+		{
+			$xpath .= '+concat("0",' . $nodePath . '/scopes/' . $scopeDim . '/@_' . $scopeVal . ')';
+		}
+
+		$xpath .= ',1)';
+
+		return $xpath;
+	}
+}
