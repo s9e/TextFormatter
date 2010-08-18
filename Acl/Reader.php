9c6aefa667b5cc29e3a8fcf6ffa646179471f5a2
Your Name <s9e.dev@gmail.com>
2010-08-18 19:16:19 +0200

    Removed duplicate code

--- Acl/Reader.php
+++ Acl/Reader.php
@@ -28,6 +28,94 @@ class Reader
 
 	public function isAllowed($perm, $scope = null)
 	{
+		$n = $this->getBitNumber($perm, $scope);
+
+		if ($n === false)
+		{
+			return false;
+		}
+
+		return (bool) (ord($this->config[$perm]['mask'][$n >> 3]) & (1 << (7 - ($n & 7))));
+	}
+
+	public function getPredicate($perm, $dim, $scope = null)
+	{
+		if (!isset($this->config[$perm]))
+		{
+			return array('type' => 'none');
+		}
+
+		$space  = $this->config[$perm];
+
+		if ($scope === ANY)
+		{
+			$scope = array_fill_keys(array_keys($space['any']), ANY);
+			unset($scope[$dim]);
+
+			$global = $this->isAllowed($perm);
+		}
+		else
+		{
+			$global = $this->isAllowed($perm, $scope);
+		}
+
+		if (!isset($space['scopes'][$dim]))
+		{
+			// That scope doesn't exist, rely on global setting
+			return array('type' => ($global) ? 'all' : 'none');
+		}
+
+		if (is_array($scope) && isset($scope[$dim]))
+		{
+			throw new \InvalidArgumentException('$scope contains the same key we are looking for');
+		}
+
+		$n = $this->getBitNumber($perm, $scope);
+
+		$allowed = $denied = array();
+		foreach ($space['scopes'][$dim] as $scopeVal => $pos)
+		{
+			$_n = $n + $pos;
+
+			if (ord($space['mask'][$_n >> 3]) & (1 << (7 - ($_n & 7))))
+			{
+				$allowed[] = $scopeVal;
+			}
+			else
+			{
+				$denied[]  = $scopeVal;
+			}
+		}
+
+		if ($global)
+		{
+			if (empty($denied))
+			{
+				return array('type' => 'all');
+			}
+			else
+			{
+				return array(
+					'type'  => 'all_but',
+					'which' => $denied
+				);
+			}
+		}
+		elseif (!empty($allowed))
+		{
+			return array(
+				'type'  => 'some',
+				'which' => $allowed
+			);
+		}
+		else
+		{
+			return array('type' => 'none');
+		}
+	}
+
+	protected function getBitNumber($perm, &$scope)
+	{
 		if (!isset($this->config[$perm]))
 		{
 			return false;
@@ -82,126 +170,6 @@ class Reader
 			}
 		}
 
-		return (bool) (ord($space['mask'][$n >> 3]) & (1 << (7 - ($n & 7))));
-	}
-
-	public function getPredicate($perm, $dim, $scope = null)
-	{
-		if (!isset($this->config[$perm]))
-		{
-			return array('type' => 'none');
-		}
-
-		$global = $this->isAllowed($perm, $scope);
-		$space  = $this->config[$perm];
-
-		if (!isset($space['scopes'][$dim]))
-		{
-			// That scope doesn't exist, rely on global setting
-			return array('type' => ($global) ? 'all' : 'none');
-		}
-
-		$n      = $space['perms'][$perm];
-
-		if (isset($scope))
-		{
-			if ($scope instanceof Resource)
-			{
-				$scope = $scope->getAclReaderScope();
-			}
-
-			if (is_array($scope))
-			{
-				if (isset($scope[$dim]))
-				{
-					throw new \InvalidArgumentException('$scope contains the same key we are looking for');
-				}
-
-				foreach ($scope as $scopeDim => $scopeVal)
-				{
-					if (is_float($scopeVal))
-					{
-						/**
-						* Doubles need to be converted to strings. Booleans should be fine
-						*/
-						$scopeVal = (string) $scopeVal;
-					}
-
-					if ($scopeVal === ANY)
-					{
-						if (isset($space['any'][$scopeDim]))
-						{
-							$n += $space['any'][$scopeDim];
-						}
-					}
-					elseif (isset($space['scopes'][$scopeDim][$scopeVal]))
-					{
-						$n += $space['scopes'][$scopeDim][$scopeVal];
-					}
-				}
-			}
-			elseif ($scope === ANY)
-			{
-				$global = $this->isAllowed($perm);
-
-				if (isset($space['any']))
-				{
-					$any = $space['any'];
-					unset($any[$dim]);
-
-					$n += array_sum($any);
-				}
-			}
-			/**
-			* @codeCoverageIgnoreStart That case will normally be caught earlier in the first call
-			*                          to isAllowed()
-			*/
-			else
-			{
-				throw new \InvalidArgumentException('$scope is expected to be an array, an object that implements ' . __NAMESPACE__ . '\\Resource, or ' . __NAMESPACE__ . '\\ANY');
-			}
-			// @codeCoverageIgnoreEnd
-		}
-
-		$allowed = $denied = array();
-		foreach ($space['scopes'][$dim] as $scopeVal => $pos)
-		{
-			$_n = $n + $pos;
-
-			if (ord($space['mask'][$_n >> 3]) & (1 << (7 - ($_n & 7))))
-			{
-				$allowed[] = $scopeVal;
-			}
-			else
-			{
-				$denied[]  = $scopeVal;
-			}
-		}
-
-		if ($global)
-		{
-			if (empty($denied))
-			{
-				return array('type' => 'all');
-			}
-			else
-			{
-				return array(
-					'type'  => 'all_but',
-					'which' => $denied
-				);
-			}
-		}
-		elseif (!empty($allowed))
-		{
-			return array(
-				'type'  => 'some',
-				'which' => $allowed
-			);
-		}
-		else
-		{
-			return array('type' => 'none');
-		}
+		return $n;
 	}
 }
