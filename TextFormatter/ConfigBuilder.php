3b233a61c79e69c876432857ae191fe3d5a63d45
Your Name <s9e.dev@gmail.com>
2010-09-22 21:10:29 +0200

    Output HTML by default

--- TextFormatter/ConfigBuilder.php
+++ TextFormatter/ConfigBuilder.php
@@ -972,7 +972,7 @@ class ConfigBuilder
 		$xsl = '<?xml version="1.0" encoding="utf-8"?>'
 		     . "\n"
 			 . '<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">'
-			 . '<xsl:output method="xml" encoding="utf-8" />'
+			 . '<xsl:output method="html" encoding="utf-8" />'
 			 . '<xsl:template match="/m">'
 			 . '<xsl:for-each select="*">'
 			 . '<xsl:apply-templates />'
