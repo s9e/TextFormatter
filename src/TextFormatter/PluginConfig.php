fe578a7099516a36b26858348f55a784663dd8f8
Your Name <s9e.dev@gmail.com>
2011-03-29 22:04:35 +0200

    March is a great month to update copyright notices

--- src/TextFormatter/PluginConfig.php
+++ src/TextFormatter/PluginConfig.php
@@ -2,7 +2,7 @@
 
 /**
 * @package   s9e\Toolkit
-* @copyright Copyright (c) 2010 The s9e Authors
+* @copyright Copyright (c) 2010-2011 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\Toolkit\TextFormatter;
