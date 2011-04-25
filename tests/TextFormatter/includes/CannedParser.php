836a903c36c8f8b88debaeff6dfc93c804c5d001
Your Name <s9e.dev@gmail.com>
2011-04-25 14:02:49 +0200

    TextFormatter: added PluginParser test

--- tests/TextFormatter/includes/CannedParser.php
+++ tests/TextFormatter/includes/CannedParser.php
@@ -6,6 +6,13 @@ use s9e\Toolkit\TextFormatter\PluginParser;
 
 class CannedParser extends PluginParser
 {
+	public $_calledCount = 0;
+
+	public function setUp()
+	{
+		++$this->_calledCount;
+	}
+
 	public function getTags($text, array $matches)
 	{
 		return $this->config['tags'];
