993cc58748779f348bcce59c40b2c21b6d1d67f3
Your Name <s9e.dev@gmail.com>
2011-05-09 12:20:15 +0200

    TextFormatter: removed old @todo. Removed Firebug from demo. Added default message

--- scripts/generateJSParserDemo.php
+++ scripts/generateJSParserDemo.php
@@ -27,51 +27,111 @@ ob_start();
 <head>
 	<meta charset="utf-8" />
 	<title></title>
+	<style type="text/css">
+		div
+		{
+			margin-bottom: 10px;
+		}
+
+		#logdiv
+		{
+			border: dashed 1px #8af;
+		}
+
+		#logdiv,
+		#preview pre
+		{
+			font-family: sans;
+			white-space: pre-wrap;
+		}
+	</style>
 </head>
 <body>
 	<div>
 		<form>
-			<textarea></textarea>
+			<textarea cols="80" rows="15">This is a demo of the Javascript port of [url=https://github.com/s9e/Toolkit/tree/master/src/TextFormatter]s9e\TextFormatter[/url].
+
+A few BBCodes have been added such as:
+
+[list]
+	[*][b]bold[/b],
+	[*][i]italic[/i],
+	[*][color=#f05]color[/color],
+	[*]+ a few others
+[/list]
+
+The code required has been minified to a few kilobytes with [url=http://closure-compiler.appspot.com/home]Google Closure Compiler[/url] but the raw sources can be found at GitHub in the [url=https://github.com/s9e/Toolkit/tree/TextFormatter-JSParser/src/TextFormatter]TextFormatter-JSParser branch[/url].
+</textarea>
+			<br>
+			<input type="checkbox" id="rendercheck" checked="checked"><label for="rendercheck"> Render</label>
+			<input type="checkbox" id="logcheck"><label for="logcheck"> Show log</label>
 		</form>
 	</div>
-	<pre></pre>
 
-	<script type="text/javascript" src="https://getfirebug.com/firebug-lite.js#startOpened,overrideConsole"></script>
+	<div id="logdiv" style="display:none"></div>
+
+	<div id="preview"><pre></pre></div>
+
 	<script type="text/javascript"><?php echo $jsParser ?>
 
 		var text,
-			textarea = document.getElementsByTagName('textarea')[0],
-			pre = document.getElementsByTagName('pre')[0];
+			xml,
 
-		var print = {
-			debug:   console.debug,
-			warning: console.warn,
-			error:   console.error
+			textarea = document.getElementsByTagName('textarea')[0],
+			pre = document.getElementsByTagName('pre')[0],
+			rendercheck = document.getElementById('rendercheck'),
+			logcheck = document.getElementById('logcheck'),
+			logdiv = document.getElementById('logdiv'),
+
+			s = new XMLSerializer();
+
+		rendercheck.onchange = refreshOutput;
+
+		logcheck.onchange = function()
+		{
+			if (logcheck.checked)
+			{
+				logdiv.style.display = '';
+				refreshLog();
+			}
+			else
+			{
+				logdiv.style.display = 'none';
+			}
 		}
 
-		window.setInterval(function()
+		function refreshOutput()
 		{
-			if (textarea.value === text)
+			console.dir(rendercheck);
+			if (rendercheck.checked)
 			{
-				return;
+				var newPRE = document.createElement('pre');
+
+				newPRE.appendChild(
+					s9e.TextFormatter.render(xml)
+				);
+
+				pre.parentNode.replaceChild(newPRE, pre);
+				pre = newPRE;
 			}
+			else
+			{
+				pre.textContent = s.serializeToString(xml)
+			}
+		}
 
-			text = textarea.value;
+		function refreshLog()
+		{
+			var type,
+				log = s9e.TextFormatter.getLog(),
+				msgs = [];
 
-			var newPRE = document.createElement('pre'),
-				xml = s9e.TextFormatter.parse(text);
-
-			newPRE.appendChild(
-				s9e.TextFormatter.render(xml)
-			);
-
-			var type, log = s9e.TextFormatter.getLog();
 			for (type in log)
 			{
 				log[type].forEach(function(entry)
 				{
-					print[type](
-						entry.msg.replace(
+					msgs.push(
+						'[' + type + '] ' + entry.msg.replace(
 							/%([0-9]\$)?[sd]/g,
 							function(m)
 							{
@@ -82,8 +142,25 @@ ob_start();
 				});
 			}
 
-			pre.parentNode.replaceChild(newPRE, pre)
-			pre = newPRE
+			logdiv.innerHTML = (msgs.length) ? msgs.join("\n") : 'No log';
+		}
+
+		window.setInterval(function()
+		{
+			if (textarea.value === text)
+			{
+				return;
+			}
+
+			text = textarea.value;
+			xml = s9e.TextFormatter.parse(text);
+
+			refreshOutput();
+
+			if (logcheck.checked)
+			{
+				refreshLog();
+			}
 		}, 50);
 	</script>
 </body>
--- src/TextFormatter/JSParserGenerator.php
+++ src/TextFormatter/JSParserGenerator.php
@@ -301,9 +301,6 @@ class JSParserGenerator
 
 			$tagsConfig .= $prepend . '"' . $tagName . '":';
 			$tagsConfig .= preg_replace(
-				/**
-				* @todo must preserve keys used in rules
-				*/
 				'#(?<=[\\{,])"([a-z]+)"(?=[:\\}])#i',
 				'$1',
 				json_encode($tagConfig, JSON_HEX_QUOT)
