b4ed86cd7eb3d979ef61bc294d346143490e6a7e
Your Name <s9e.dev@gmail.com>
2011-05-22 16:49:01 +0200

    TextFormatter: added PluginConfig's JS-related tests

--- tests/TextFormatter/PluginConfigTest.php
+++ tests/TextFormatter/PluginConfigTest.php
@@ -20,4 +20,46 @@ class PluginConfigTest extends Test
 
 		$this->assertObjectHasAttribute('foo', $plugin);
 	}
+
+	/**
+	* @test
+	*/
+	public function getJSConfig_forwards_the_result_of_getConfig_by_default()
+	{
+		include_once __DIR__ . '/includes/CannedConfig.php';
+
+		$plugin = new CannedConfig($this->cb);
+
+		$this->assertSame(
+			$plugin->getConfig(),
+			$plugin->getJSConfig()
+		);
+	}
+
+	/**
+	* @test
+	*/
+	public function getJSConfigMeta_returns_an_empty_array_by_default()
+	{
+		include_once __DIR__ . '/includes/CannedConfig.php';
+
+		$plugin = new CannedConfig($this->cb);
+
+		$this->assertSame(
+			array(),
+			$plugin->getJSConfigMeta()
+		);
+	}
+
+	/**
+	* @test
+	*/
+	public function getJSParser_returns_false_by_default()
+	{
+		include_once __DIR__ . '/includes/CannedConfig.php';
+
+		$plugin = new CannedConfig($this->cb);
+
+		$this->assertFalse($plugin->getJSParser());
+	}
 }
