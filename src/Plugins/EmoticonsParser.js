f521f18776b5fa3579acef5c1b08c663b5263712
Your Name <s9e.dev@gmail.com>
2011-07-20 00:31:22 +0200

    Minor change

--- src/Plugins/EmoticonsParser.js
+++ src/Plugins/EmoticonsParser.js
@@ -1,11 +1,12 @@
-var tags = [];
+var tags = [],
+	tagName = config.tagName;
 
 matches.forEach(function(m)
 {
 	tags.push({
 		pos  : m[0][1],
 		type : SELF_CLOSING_TAG,
-		name : config.tagName,
+		name : tagName,
 		len  : m[0][0].length
 	});
 });
