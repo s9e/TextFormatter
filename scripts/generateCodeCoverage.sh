5c1a181b77316b6ab06487a2569b66b86ef9ccaa
Your Name <s9e.dev@gmail.com>
2011-07-20 04:56:11 +0200

    Changed target dir for code coverage output

--- scripts/generateCodeCoverage.sh
+++ scripts/generateCodeCoverage.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 DIR=$(dirname $(dirname $(realpath $0)))
+TARGET=$(dirname $DIR)/s9e.github.com/TextFormatter/coverage
 
 cd $DIR/tests
 
 rm -f $DIR/docs/coverage/*
-phpunit -d memory_limit=256M -c $DIR/tests/phpunit.xml --coverage-html $DIR/docs/coverage
+phpunit -d memory_limit=256M -c $DIR/tests/phpunit.xml --coverage-html $TARGET
 
 REGEXP=s/`echo $(dirname $(dirname $DIR)) | sed -e 's/\\//\\\\\//g'`//g
-sed -i $REGEXP $DIR/docs/coverage/*.html
+sed -i $REGEXP $TARGET/*.html
