7764f2544df613d3ab576be5491d285ddc12a02f
Your Name <s9e.dev@gmail.com>
2011-07-20 08:35:55 +0200

    Replace generation time with generation tree

--- scripts/generateCodeCoverage.sh
+++ scripts/generateCodeCoverage.sh
@@ -9,3 +9,7 @@ phpunit -d memory_limit=256M -c $DIR/tests/phpunit.xml --coverage-html $TARGET
 
 REGEXP=s/`echo $(dirname $(dirname $DIR)) | sed -e 's/\\//\\\\\//g'`//g
 sed -i $REGEXP $TARGET/*.html
+
+SHA1=`git rev-parse HEAD`
+REGEXP='s/(<td class="versionInfo">.*?at )[^.]+/\1<a href="https:\/\/github.com\/s9e\/TextFormatter\/tree\/'$SHA1'">'$SHA1'<\/a>/'
+sed -i -r "$REGEXP" $TARGET/*.html
