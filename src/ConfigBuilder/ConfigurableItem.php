ede1279729055111f69117b920b18a45a71f0d38
Your Name <s9e.dev@gmail.com>
2012-02-08 16:41:56 +0100

    WiP

--- src/ConfigBuilder/ConfigurableItem.php
+++ src/ConfigBuilder/ConfigurableItem.php
@@ -7,6 +7,8 @@
 */
 namespace s9e\TextFormatter\ConfigBuilder;
 
+use InvalidArgumentException;
+
 abstract class ConfigurableItem implements Configurable, Item
 {
 	/**
@@ -67,6 +69,11 @@ abstract class ConfigurableItem implements Configurable, Item
 		if (method_exists($this, $methodName))
 		{
 			return $this->$methodName($optionValue);
+		}
+
+		if (!property_exists($this, $optionName))
+		{
+			throw new InvalidArgumentException("Option '" . $optionName . "' does not exist");
 		}
 
 		// If the property already exists, preserve its type
