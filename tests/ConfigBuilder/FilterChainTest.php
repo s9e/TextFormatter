75841630de493828c33e78b3aba1d7399ff58fc4
Your Name <s9e.dev@gmail.com>
2012-02-09 08:06:14 +0100

    Done with FilterChain

--- tests/ConfigBuilder/FilterChainTest.php
+++ tests/ConfigBuilder/FilterChainTest.php
@@ -4,6 +4,7 @@ namespace s9e\TextFormatter\Tests\ConfigBuilder;
 
 use Exception,
     s9e\TextFormatter\Tests\Test,
+    s9e\TextFormatter\ConfigBuilder\Filter,
     s9e\TextFormatter\ConfigBuilder\FilterChain;
 
 include_once __DIR__ . '/../../src/autoloader.php';
@@ -13,22 +14,156 @@ include_once __DIR__ . '/../../src/autoloader.php';
 */
 class FilterChainTest extends Test
 {
-	/**
-	* @testdox append('#int')
-	*/
-	public function testAppendDefaultFilter()
+	static public function staticMethod() {}
+	public function method() {}
+
+	public function setUp()
 	{
-		$filterChain = new FilterChain(array('attrVal' => null));
-		$filterChain->append('#int');
+		$this->filterChain = new FilterChain(array('attrVal' => null));
 	}
 
 	/**
-	* @testdox append('strtolower')
+	* @testdox append() and prepend() accept default filters such as '#int'
+	*/
+	public function testAppendDefaultFilter()
+	{
+		$this->filterChain->append('#int');
+		$this->filterChain->prepend('#int');
+	}
+
+	/**
+	* @testdox append() and prepend() accept simple callbacks such as 'strtolower'
 	*/
 	public function testAppendStringCallback()
 	{
-		$filterChain = new FilterChain(array('attrVal' => null));
-		$filterChain->append('strtolower');
+		$this->filterChain->append('strtolower');
+		$this->filterChain->prepend('strtolower');
+	}
+
+	/**
+	* @testdox append() and prepend() accept static class method calls such as array('Class', 'staticMethod')
+	*/
+	public function testAppendStaticMethodCallback()
+	{
+		$this->filterChain->append(array(__CLASS__, 'staticMethod'));
+		$this->filterChain->prepend(array(__CLASS__, 'staticMethod'));
+	}
+
+	/**
+	* @testdox append() and prepend() accept static class method calls such as array('Class::staticMethod')
+	*/
+	public function testAppendStaticMethodAsStringCallback()
+	{
+		$this->filterChain->append(__CLASS__ . '::staticMethod');
+		$this->filterChain->prepend(__CLASS__ . '::staticMethod');
+	}
+
+	/**
+	* @testdox append() and prepend() accept object method calls such as array($this, 'method')
+	*/
+	public function testAppendObjectMethodCallback()
+	{
+		$this->filterChain->append(array($this, 'method'));
+		$this->filterChain->prepend(array($this, 'method'));
+	}
+
+	/**
+	* @testdox append() and prepend() accept Filter objects
+	*/
+	public function testAppendFilterObject()
+	{
+		$this->filterChain->append(new Filter('strtolower'));
+		$this->filterChain->prepend(new Filter('strtolower'));
+	}
+
+	/**
+	* @testdox append() and prepend() accept closures
+	*/
+	public function testAppendClosure()
+	{
+		$this->filterChain->append(function() {});
+		$this->filterChain->prepend(function() {});
+	}
+
+	/**
+	* @testdox append() throws a InvalidArgumentException if its argument is not callable
+	* @expectedException InvalidArgumentException
+	* @expectedExceptionMessage Callback '1' is not callable
+	*/
+	public function testAppendException()
+	{
+		$this->filterChain->append(1);
+	}
+
+	/**
+	* @testdox prepend() throws a InvalidArgumentException if its argument is not callable
+	* @expectedException InvalidArgumentException
+	* @expectedExceptionMessage Callback '1' is not callable
+	*/
+	public function testPrependException()
+	{
+		$this->filterChain->prepend(1);
+	}
+
+	/**
+	* @testdox get() returns all filters in an array
+	*/
+	public function testGet()
+	{
+		$this->assertSame(
+			array(),
+			$this->filterChain->get()
+		);
+	}
+
+	/**
+	* @testdox get() returns default filters such as '#int' as strings
+	*/
+	public function testGetDefaultFilter()
+	{
+		$this->filterChain->append('#int');
+
+		$this->assertSame(
+			array('#int'),
+			$this->filterChain->get()
+		);
+	}
+
+	/**
+	* @testdox get() returns other filters as Filter objects
+	*/
+	public function testGetOtherFilters()
+	{
+		$this->filterChain->append('strtolower');
+		$this->filterChain->append(array($this, 'method'));
+		$this->filterChain->append(function() {});
+
+		$get = $this->filterChain->get();
+
+		$this->assertSame(3, count($get));
+
+		foreach ($get as $filter)
+		{
+			$this->assertInstanceOf(
+				's9e\\TextFormatter\\ConfigBuilder\\Filter',
+				$filter
+			);
+		}
+	}
+
+	/**
+	* @testdox clear() removes all filters
+	*/
+	public function testClear()
+	{
+		$this->filterChain->append('#int');
+		$this->filterChain->clear();
+
+		$this->assertSame(
+			array(),
+			$this->filterChain->get()
+		);
+
 	}
 
 	/**
@@ -36,21 +171,19 @@ class FilterChainTest extends Test
 	*/
 	public function testHasDefaultFilterTrue()
 	{
-		$filterChain = new FilterChain(array('attrVal' => null));
-		$filterChain->append('#int');
+		$this->filterChain->append('#int');
 
-		$this->assertTrue($filterChain->has('#int'));
+		$this->assertTrue($this->filterChain->has('#int'));
 	}
 
 	/**
-	* @testdox has('#int') returns false if only '#number' is in the chain
+	* @testdox has('#int') returns false if '#number' only is in the chain
 	*/
 	public function testHasDefaultFilterFalse()
 	{
-		$filterChain = new FilterChain(array('attrVal' => null));
-		$filterChain->append('#number');
+		$this->filterChain->append('#number');
 
-		$this->assertFalse($filterChain->has('#int'));
+		$this->assertFalse($this->filterChain->has('#int'));
 	}
 
 	/**
@@ -58,20 +191,76 @@ class FilterChainTest extends Test
 	*/
 	public function testHasStringCallbackTrue()
 	{
-		$filterChain = new FilterChain(array('attrVal' => null));
-		$filterChain->append('strtolower');
+		$this->filterChain->append('strtolower');
 
-		$this->assertTrue($filterChain->has('strtolower'));
+		$this->assertTrue($this->filterChain->has('strtolower'));
 	}
 
 	/**
-	* @testdox has('strtolower') returns false if 'trim' only is in the chain
+	* @testdox has('strtolower') returns false if only 'trim' is in the chain
 	*/
 	public function testHasStringCallbackFalse()
 	{
-		$filterChain = new FilterChain(array('attrVal' => null));
-		$filterChain->append('trim');
+		$this->filterChain->append('trim');
 
-		$this->assertFalse($filterChain->has('strtolower'));
+		$this->assertFalse($this->filterChain->has('strtolower'));
+	}
+
+	/**
+	* @testdox has($FilterObject) returns true if $FilterObject is in the chain
+	*/
+	public function testHasFilterObjectTrue()
+	{
+		$FilterObject = new Filter('trim');
+
+		$this->filterChain->append($FilterObject);
+
+		$this->assertTrue($this->filterChain->has($FilterObject));
+	}
+
+	/**
+	* @testdox has($FilterObject) returns true if $FilterObjectClone is in the chain
+	*/
+	public function testHasFilterObjectCloneTrue()
+	{
+		$FilterObject      = new Filter('trim');
+		$FilterObjectClone = clone $FilterObject;
+
+		$this->filterChain->append($FilterObjectClone);
+
+		$this->assertTrue($this->filterChain->has($FilterObject));
+	}
+
+	/**
+	* @testdox has($FilterObject) returns false if only $SomeOtherFilterObject is in the chain
+	*/
+	public function testHasFilterObjectFalse()
+	{
+		$FilterObject          = new Filter('trim');
+		$SomeOtherFilterObject = new Filter('strtolower');
+
+		$this->filterChain->append($SomeOtherFilterObject);
+
+		$this->assertFalse($this->filterChain->has($FilterObject));
+	}
+
+	/**
+	* @testdox FilterChain is iterable with foreach, with filter position key and filter as value
+	*/
+	public function testForeach()
+	{
+		$this->filterChain->append('#int');
+		$this->filterChain->append('#number');
+
+		$filters = array();
+		foreach ($this->filterChain as $k => $v)
+		{
+			$filters[$k] = $v;
+		}
+
+		$this->assertSame(
+			array('#int', '#number'),
+			$filters
+		);
 	}
 }
