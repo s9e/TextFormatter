800012fdaf6fb7d2c83b66105e3b4a40956240b6
Your Name <s9e.dev@gmail.com>
2012-10-24 15:51:32 +0200

    WiP

--- tests/Plugins/ConfiguratorBaseTest.php
+++ tests/Plugins/ConfiguratorBaseTest.php
@@ -37,6 +37,66 @@ class ConfiguratorBaseTest extends Test
 	{
 		new DummyPluginConfigurator($this->configurator, array('baz' => 'baz'));
 	}
+
+	/**
+	* @testdox setRegexpLimit() sets the regexpLimit property
+	*/
+	public function testSetRegexpLimit()
+	{
+		$dummy = new DummyPluginConfigurator($this->configurator);
+		$dummy->setRegexpLimit(1234);
+		$this->assertAttributeEquals(1234, 'regexpLimit', $dummy);
+	}
+
+	/**
+	* @testdox setRegexpLimit() throws an exception on invalid values
+	* @expectedException InvalidArgumentException
+	* @expectedExceptionMessage regexpLimit must be a number greater than 0
+	*/
+	public function testSetRegexpLimitInvalid()
+	{
+		$dummy = new DummyPluginConfigurator($this->configurator);
+		$dummy->setRegexpLimit(null);
+	}
+
+	/**
+	* @testdox setRegexpLimitAction() sets the regexpLimitAction property
+	*/
+	public function testSetRegexpLimitAction()
+	{
+		$dummy = new DummyPluginConfigurator($this->configurator);
+		$dummy->setRegexpLimitAction('abort');
+		$this->assertAttributeEquals('abort', 'regexpLimitAction', $dummy);
+	}
+
+	/**
+	* @testdox setRegexpLimitAction() throws an exception on invalid values
+	* @expectedException InvalidArgumentException
+	* @expectedExceptionMessage regexpLimitAction must be any of: 'ignore', 'warn' or 'abort'
+	*/
+	public function testSetRegexpLimitActionInvalid()
+	{
+		$dummy = new DummyPluginConfigurator($this->configurator);
+		$dummy->setRegexpLimitAction('chill out');
+	}
+
+	/**
+	* @testdox Offers a default toConfig() implementation that leaves out the configurator instance
+	*/
+	public function testToConfig()
+	{
+		$dummy = new DummyPluginConfigurator($this->configurator);
+
+		$this->assertEquals(
+			array(
+				'foo' => 'foo',
+				'bar' => 'foo',
+				'regexpLimit' => 1000,
+				'regexpLimitAction' => 'ignore'
+			),
+			$dummy->toConfig()
+		);
+	}
 }
 
 class DummyPluginConfigurator extends ConfiguratorBase
