c97e7806a8d51606335792ec13963e5ca10d0853
Your Name <s9e.dev@gmail.com>
2012-12-05 16:25:14 +0100

    WiP

---
+++ tests/ParserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace s9e\TextFormatter\Tests;
+
+use s9e\TextFormatter\Parser;
+use s9e\TextFormatter\Tests\Test;
+
+/**
+* @covers s9e\TextFormatter\Parser
+*/
+class ParserTest extends Test
+{
+}
