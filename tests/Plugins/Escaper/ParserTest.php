0329b6ed9cce35d701e98d6f9618986ed010152e
s9e <s9e.dev@gmail.com>
2012-12-19 20:02:21 +0100

    WiP

---
+++ tests/Plugins/Escaper/ParserTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace s9e\TextFormatter\Tests\Plugins\Escaper;
+
+use s9e\TextFormatter\Configurator;
+use s9e\TextFormatter\Plugins\Escaper\Parser;
+use s9e\TextFormatter\Tests\Plugins\ParsingTestsRunner;
+use s9e\TextFormatter\Tests\Plugins\RenderingTestsRunner;
+use s9e\TextFormatter\Tests\Test;
+
+/**
+* @covers s9e\TextFormatter\Plugins\Escaper\Parser
+*/
+class ParserTest extends Test
+{
+	use ParsingTestsRunner;
+	use RenderingTestsRunner;
+
+	public function getParsingTests()
+	{
+		return array(
+			array(
+				'\\[',
+				'<rt><ESC>\\[</ESC></rt>'
+			),
+			array(
+				'\\[',
+				'<rt><FOO>\\[</FOO></rt>',
+				array('tagName' => 'FOO')
+			),
+			array(
+				"a\\\nb",
+				"<rt>a<ESC>\\\n</ESC>b</rt>"
+			),
+			array(
+				'a\\♥b',
+				'<rt>a<ESC>\\♥</ESC>b</rt>'
+			),
+		);
+	}
+
+	public function getRenderingTests()
+	{
+		return array(
+			array(
+				'\\[',
+				'['
+			),
+			array(
+				'\\[',
+				'[',
+				array('tagName' => 'FOO')
+			),
+			array(
+				"a\\\nb",
+				"a\nb"
+			),
+			array(
+				'a\\♥b',
+				'a♥b'
+			),
+		);
+	}
+}
