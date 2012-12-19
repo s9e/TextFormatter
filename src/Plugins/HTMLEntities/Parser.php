685028587859b185da0706eaa8d3989c5858e3b3
s9e <s9e.dev@gmail.com>
2012-12-19 19:15:44 +0100

    WiP

--- src/Plugins/HTMLEntities/Parser.php
+++ src/Plugins/HTMLEntities/Parser.php
@@ -5,40 +5,38 @@
 * @copyright Copyright (c) 2010-2012 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
-namespace s9e\TextFormatter\Plugins;
+namespace s9e\TextFormatter\Plugins\HTMLEntities;
 
-use s9e\TextFormatter\Parser;
-use s9e\TextFormatter\PluginParser;
+use s9e\TextFormatter\Plugins\ParserBase;
 
-class HTMLEntitiesParser extends PluginParser
+class Parser extends ParserBase
 {
-	public function getTags($text, array $matches)
+	/**
+	* {@inheritDoc}
+	*/
+	public function parse($text, array $matches)
 	{
-		$tags = array();
+		$tagName  = $this->config['tagName'];
+		$attrName = $this->config['attrName'];
 
 		foreach ($matches as $m)
 		{
-			if (isset($this->config['disabled'][$m[0][0]]))
+			$entity = $m[0][0];
+
+			if (isset($this->config['disabled'][$entity]))
 			{
 				continue;
 			}
 
-			$char = html_entity_decode($m[0][0], ENT_QUOTES, 'UTF-8');
+			$char = html_entity_decode($entity, ENT_QUOTES, 'UTF-8');
 
-			if ($char === $m[0][0])
+			if ($char === $entity)
 			{
+				// The entity was not decoded, so we assume it's not valid and we ignore it
 				continue;
 			}
 
-			$tags[] = array(
-				'pos'   => $m[0][1],
-				'type'  => Parser::SELF_CLOSING_TAG,
-				'name'  => $this->config['tagName'],
-				'len'   => strlen($m[0][0]),
-				'attrs' => array($this->config['attrName'] => $char)
-			);
+			$this->parser->addSelfClosingTag($tagName, $m[0][1], strlen($entity))->setAttribute($attrName, $char);
 		}
-
-		return $tags;
 	}
 }
