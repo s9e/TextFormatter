a1977d57ff78de2a29f31db13d05ed832175c8e1
s9e <s9e.dev@gmail.com>
2012-12-19 19:05:11 +0100

    WiP

--- tests/Plugins/Censor/ParserTest.php
+++ tests/Plugins/Censor/ParserTest.php
@@ -46,6 +46,114 @@ class ParserTest extends Test
 					$constructor->Censor->add('apple', 'orange');
 				},
 			),
+			array(
+				'You dirty 苹果',
+				'<rt>You dirty <CENSOR with="orange">苹果</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('苹果', 'orange');
+				}
+			),
+			array(
+				'You dirty apple',
+				'<rt>You dirty <CENSOR>apple</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('*pple');
+				}
+			),
+			array(
+				'You dirty apple',
+				'<rt>You dirty <CENSOR>apple</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('ap*e');
+				}
+			),
+			array(
+				'You dirty apple',
+				'<rt>You dirty <CENSOR>apple</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('app*');
+				}
+			),
+			array(
+				'You dirty apple',
+				'<rt>You dirty <CENSOR>apple</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('*apple*');
+				}
+			),
+			array(
+				'You dirty Pokéman',
+				'<rt>You dirty <CENSOR>Pokéman</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('pok*man');
+				}
+			),
+			array(
+				'You dirty Pok3man',
+				'<rt>You dirty <CENSOR>Pok3man</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('pok?man');
+				}
+			),
+			array(
+				'You dirty Pok#man',
+				'<rt>You dirty <CENSOR>Pok#man</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('pok?man');
+				}
+			),
+			array(
+				'You dirty Pokéman',
+				'<rt>You dirty <CENSOR with="digiman">Pokéman</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('pok?man', 'digiman');
+				}
+			),
+			array(
+				'You dirty apple',
+				'<rt>You dirty <CENSOR>apple</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('?pple');
+				}
+			),
+			array(
+				'You dirty pineapple',
+				'<pt>You dirty pineapple</pt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('?pple');
+				}
+			),
+			array(
+				'You dirty $0',
+				'<rt>You dirty <CENSOR>$0</CENSOR></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->Censor->add('$0');
+				}
+			),
 		);
 	}
 
@@ -86,114 +194,6 @@ class ParserTest extends Test
 				function ($constructor)
 				{
 					$constructor->Censor->add('apple', 'orange');
-				}
-			),
-			array(
-				'You dirty 苹果',
-				'You dirty orange',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('苹果', 'orange');
-				}
-			),
-			array(
-				'You dirty apple',
-				'You dirty ****',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('*pple');
-				}
-			),
-			array(
-				'You dirty apple',
-				'You dirty ****',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('ap*e');
-				}
-			),
-			array(
-				'You dirty apple',
-				'You dirty ****',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('app*');
-				}
-			),
-			array(
-				'You dirty apple',
-				'You dirty ****',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('*apple*');
-				}
-			),
-			array(
-				'You dirty Pokéman',
-				'You dirty ****',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('pok*man');
-				}
-			),
-			array(
-				'You dirty Pok3man',
-				'You dirty ****',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('pok?man');
-				}
-			),
-			array(
-				'You dirty Pok#man',
-				'You dirty ****',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('pok?man');
-				}
-			),
-			array(
-				'You dirty Pokéman',
-				'You dirty digiman',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('pok?man', 'digiman');
-				}
-			),
-			array(
-				'You dirty apple',
-				'You dirty ****',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('?pple');
-				}
-			),
-			array(
-				'You dirty pineapple',
-				'You dirty pineapple',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('?pple');
-				}
-			),
-			array(
-				'You dirty $0',
-				'You dirty ****',
-				array(),
-				function ($constructor)
-				{
-					$constructor->Censor->add('$0');
 				}
 			),
 		);
