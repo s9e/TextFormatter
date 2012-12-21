11279326ac6431c4676252df924dd9f813b0945a
s9e <s9e.dev@gmail.com>
2012-12-21 02:26:59 +0100

    WiP

--- tests/Plugins/BBCodes/ParserTest.php
+++ tests/Plugins/BBCodes/ParserTest.php
@@ -38,6 +38,56 @@ class ParserTest extends Test
 				},
 			),
 			array(
+				'x [B/] y',
+				'<rt>x <B>[B/]</B> y</rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$constructor->tags->add('B');
+				},
+			),
+			array(
+				'x [B /] y',
+				'<rt>x <B>[B /]</B> y</rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$constructor->tags->add('B');
+				},
+			),
+			array(
+				'x [B/[',
+				'<pt>x [B/[</pt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$constructor->tags->add('B');
+				},
+			),
+			array(
+				'x [B/',
+				'<pt>x [B/</pt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$constructor->tags->add('B');
+				},
+			),
+			array(
+				'x [B  ',
+				'<pt>x [B  </pt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$constructor->tags->add('B');
+				},
+			),
+			array(
 				'x [b]bold[/b] y',
 				'<rt>x <FOO><st>[b]</st>bold<et>[/b]</et></FOO> y</rt>',
 				array(),
@@ -50,6 +100,16 @@ class ParserTest extends Test
 			array(
 				'x [b y="foo"]bold[/b] y',
 				'<rt>x <B y="foo"><st>[b y="foo"]</st>bold<et>[/b]</et></B> y</rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$constructor->tags->add('B')->attributes->add('y');
+				},
+			),
+			array(
+				'x [b Y="foo"]bold[/b] y',
+				'<rt>x <B y="foo"><st>[b Y="foo"]</st>bold<et>[/b]</et></B> y</rt>',
 				array(),
 				function ($constructor)
 				{
@@ -91,6 +151,173 @@ class ParserTest extends Test
 					$attributes = $constructor->tags->add('B')->attributes;
 					$attributes->add('x');
 					$attributes->add('y');
+				},
+			),
+			array(
+				"x [b x='\"bar\"'/] y",
+				'<rt>x <B x="&quot;bar&quot;">[b x=\'"bar"\'/]</B> y</rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				'x [b x="\'bar\'"/] y',
+				'<rt>x <B x="\'bar\'">[b x="\'bar\'"/]</B> y</rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				'x [b x="\\""/] y',
+				'<rt>x <B x="&quot;">[b x="\\""/]</B> y</rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				"x [b x='\\''/] y",
+				"<rt>x <B x=\"'\">[b x='\\''/]</B> y</rt>",
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				'x [b x="\\\\\\""/] y',
+				'<rt>x <B x="\\&quot;">[b x="\\\\\\""/]</B> y</rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				'x [b x=" ] y',
+				'<pt>x [b x=" ] y</pt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				"x [b x=' ] y",
+				"<pt>x [b x=' ] y</pt>",
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				'x [b x!',
+				'<pt>x [b x!</pt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				'x [b x]',
+				'<rt>x <B><st>[b x]</st></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				'x [b foo x=1]',
+				'<rt>x <B x="1"><st>[b foo x=1]</st></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+					$attributes->add('foo');
+				},
+			),
+			array(
+				'x [b x',
+				'<pt>x [b x</pt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				'x [b x=',
+				'<pt>x [b x=</pt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				'x [b x=bar',
+				'<pt>x [b x=bar</pt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('x');
+				},
+			),
+			array(
+				'x [B="foo" /]',
+				'<rt>x <B b="foo">[B="foo" /]</B></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B');
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('b');
+				},
+			),
+			array(
+				'x [b="foo" /]',
+				'<rt>x <B x="foo">[b="foo" /]</B></rt>',
+				array(),
+				function ($constructor)
+				{
+					$constructor->BBCodes->add('B')->defaultAttribute = 'x';
+					$attributes = $constructor->tags->add('B')->attributes;
+					$attributes->add('b');
+					$attributes->add('x');
 				},
 			),
 		);
