28697754f7bbcfc631c44ecbeb2bbfa9ee631b4d
s9e <s9e.dev@gmail.com>
2012-12-24 15:58:27 +0100

    WiP

--- tests/Parser/TagProcessingTest.php
+++ tests/Parser/TagProcessingTest.php
@@ -61,6 +61,85 @@ class TagProcessingTest extends Test
 					$parser->addEndTag('X', 7, 0);
 				}
 			),
+			array(
+				'foo bar',
+				'<rt>foo <X>bar</X></rt>',
+				function ($constructor)
+				{
+					$constructor->tags->add('X');
+				},
+				function ($parser)
+				{
+					$parser->addStartTag('X', 4, 0);
+				}
+			),
+			array(
+				'foo bar',
+				'<pt>foo bar</pt>',
+				null,
+				function ($parser)
+				{
+					$parser->addStartTag('X', 4, 0);
+				}
+			),
+			array(
+				'foo bar',
+				'<rt>foo <X>bar</X></rt>',
+				function ($constructor)
+				{
+					$constructor->tags->add('X');
+					$constructor->tags->add('Y');
+				},
+				function ($parser)
+				{
+					$parser->addStartTag('X', 4, 0);
+					$parser->addEndTag('Y', 5, 0);
+				}
+			),
+			array(
+				'foo bar',
+				'<rt><X>foo</X> bar</rt>',
+				function ($constructor)
+				{
+					$constructor->tags->add('X');
+				},
+				function ($parser)
+				{
+					$parser->addSelfClosingTag('X', 0, 3);
+					$parser->addSelfClosingTag('X', 0, 3);
+					$parser->addSelfClosingTag('X', 1, 1);
+				}
+			),
+			array(
+				'fooo bar',
+				'<rt><X>f<X>oo</X>o</X> bar</rt>',
+				function ($constructor)
+				{
+					$constructor->tags->add('X')->nestingLimit = 2;
+				},
+				function ($parser)
+				{
+					$parser->addStartTag('X', 0, 0);
+					$parser->addStartTag('X', 1, 0);
+					$parser->addSelfClosingTag('X', 2, 1);
+					$parser->addEndTag('X', 3, 0);
+					$parser->addEndTag('X', 4, 0);
+				}
+			),
+			array(
+				'foo bar',
+				'<rt><X>f</X><X>o</X>o bar</rt>',
+				function ($constructor)
+				{
+					$constructor->tags->add('X')->tagLimit = 2;
+				},
+				function ($parser)
+				{
+					$parser->addSelfClosingTag('X', 0, 1);
+					$parser->addSelfClosingTag('X', 1, 1);
+					$parser->addSelfClosingTag('X', 2, 1);
+				}
+			),
 		);
 	}
 }
