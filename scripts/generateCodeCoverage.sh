9c72f45c830e33743c3479e9dab28f171ef9164f
s9e <s9e.dev@gmail.com>
2013-01-01 19:51:11 +0100

    WiP

--- scripts/generateCodeCoverage.sh
+++ scripts/generateCodeCoverage.sh
@@ -2,10 +2,11 @@
 DIR=$(dirname $(dirname $(realpath $0)))
 TARGET=$(dirname $DIR)/s9e.github.com/TextFormatter/coverage
 
-cd $DIR/tests
+cd $DIR
+ls -l
 
-rm -rf $TARGET
-phpunit -d memory_limit=256M -c $DIR/tests/phpunit.xml --coverage-html $TARGET
+echo rm -rf $TARGET
+phpunit -d memory_limit=256M -c phpunit.xml --coverage-html $TARGET
 
 REGEXP=s/`echo $(dirname $(dirname $DIR)) | sed -e 's/\\//\\\\\//g'`//g
 sed -i $REGEXP $TARGET/*.html
