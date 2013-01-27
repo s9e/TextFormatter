d796cda0cef32efe339ba4993af6cbe1c7e4bda5
s9e <s9e.dev@gmail.com>
2013-01-27 23:13:00 +0100

    Added a proxy for the Closure Compiler Service tests

--- tests/Configurator/JavaScript/Minifiers/ClosureCompilerServiceTest.php
+++ tests/Configurator/JavaScript/Minifiers/ClosureCompilerServiceTest.php
@@ -10,11 +10,21 @@ use s9e\TextFormatter\Tests\Test;
 */
 class ClosureCompilerServiceTest extends Test
 {
+	public function setUp()
+	{
+		stream_wrapper_unregister('http');
+		stream_wrapper_register('http', __NAMESPACE__ . '\\ClosureCompilerServiceProxy');
+	}
+
+	public function tearDown()
+	{
+		stream_wrapper_restore('http');
+	}
+
 	/**
-	* @testdox
-	* @group needs-network
+	* @testdox Works
 	*/
-	public function test()
+	public function testBasic()
 	{
 		$original =
 			"function hello(name) {
@@ -26,5 +36,56 @@ class ClosureCompilerServiceTest extends Test
 
 		$minifier = new ClosureCompilerService;
 		$this->assertSame($expected, $minifier->minify($original));
+	}
+}
+
+class ClosureCompilerServiceProxy
+{
+	protected $cacheFile;
+	protected $eof = false;
+	protected $url;
+
+	public function __construct()
+	{
+		$id = sprintf('%08X', crc32(serialize(stream_context_get_options($this->context))));
+		$this->cacheFile = __DIR__ . '/cache/' . $id;
+	}
+
+	public function stream_open($url)
+	{
+		$this->url = $url;
+
+		return true;
+	}
+
+	public function stream_stat()
+	{
+		return false;
+	}
+
+	public function stream_read()
+	{
+		if ($this->eof)
+		{
+			return '';
+		}
+		$this->eof = true;
+
+		if (file_exists($this->cacheFile))
+		{
+			return unserialize(file_get_contents($this->cacheFile));
+		}
+
+		stream_wrapper_restore('http');
+
+		$response = file_get_contents($this->url, false, $this->context);
+		file_put_contents($this->cacheFile, serialize($response));
+
+		return $response;
+	}
+
+	public function stream_eof()
+	{
+		return $this->eof;
 	}
 }
