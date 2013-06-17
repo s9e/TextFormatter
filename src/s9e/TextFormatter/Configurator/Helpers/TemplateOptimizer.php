c6a84c8c80f2287cf40b096b8e6e1de853aaeda2
s9e <s9e.dev@gmail.com>
2013-06-17 18:37:47 +0200

    TemplateOptimizer: inline values where possible

--- src/s9e/TextFormatter/Configurator/Helpers/TemplateOptimizer.php
+++ src/s9e/TextFormatter/Configurator/Helpers/TemplateOptimizer.php
@@ -54,6 +54,7 @@ abstract class TemplateOptimizer
 		self::optimizeConditionalValueOf($dom);
 		self::inlineElements($dom);
 		self::inlineAttributes($dom);
+		self::inlineValues($dom);
 		self::optimizeConditionalAttributes($dom);
 
 		// Replace <xsl:text/> elements, except for whitespace nodes
@@ -231,6 +232,93 @@ abstract class TemplateOptimizer
 			}
 
 			$attribute->parentNode->removeChild($attribute);
+		}
+	}
+
+	/**
+	* Inline the text content of a node or the value of an attribute where it's known
+	*
+	* Will replace
+	*     <xsl:if test="@foo='Foo'"><xsl:value-of select="@foo"/></xsl:if>
+	* with
+	*     Foo
+	*
+	* @param DOMDocument $dom xsl:template node
+	*/
+	protected static function inlineValues(DOMDocument $dom)
+	{
+		$xpath = new DOMXPath($dom);
+		$query = '//xsl:if | //xsl:when';
+
+		// Match an equality test between an attribute or . and a string or a number
+		$regexp = '#^(@[-\\w]+|\\.)=("[^"]*"|\'[^\']*\'|\\d+)$#';
+
+		foreach ($xpath->query($query) as $node)
+		{
+			if (!preg_match($regexp, $node->getAttribute('test'), $m))
+			{
+				continue;
+			}
+
+			$var   = $m[1];
+			$value = $m[2];
+
+			// Remove the quotes around the value
+			if ($value[0] === '"' || $value[0] === "'")
+			{
+				$value = substr($value, 1, -1);
+			}
+
+			// Get xsl:value-of descendants that match the condition
+			$query = './/xsl:value-of[@select="' . $var . '"]';
+			foreach ($xpath->query($query, $node) as $valueOf)
+			{
+				$valueOf->parentNode->replaceChild(
+					$dom->createTextNode($value),
+					$valueOf
+				);
+			}
+
+			// Get all attributes from non-XSL elements that *could* match the condition
+			$query = './/*[namespace-uri() != "' . self::XMLNS_XSL . '"]'
+			       . '/@*[contains(., "{' . $var . '}")]';
+			foreach ($xpath->query($query, $node) as $attribute)
+			{
+				$attrValue = '';
+				foreach (TemplateHelper::parseAttributeValueTemplate($attribute->value) as $token)
+				{
+					if ($token[0] === 'literal')
+					{
+						// Re-insert literals, don't forget to escape { and }
+						$attrValue .= preg_replace('/([{}])/', '$1$1', $token[1]);
+					}
+					elseif ($token[0] === 'expression')
+					{
+						// Test whether this expression is the one we're looking for
+						if ($token[1] === $var)
+						{
+							// Replace the expression with the value, don't forget to escape { and }
+							$attrValue .= preg_replace('/([{}])/', '$1$1', $value);
+						}
+						else
+						{
+							// Re-insert the expression as-is
+							$attrValue .= '{' . $token[1] . '}';
+						}
+					}
+					// @codeCoverageIgnoreStart
+					else
+					{
+						// This cannot actually happen, but if we can't interpret the token, we'll
+						// just abort
+						break 2;
+					}
+					// @codeCoverageIgnoreEnd
+				}
+
+				// Replace the attribute's value with the new inlined version
+				$attribute->value = $attrValue;
+			}
 		}
 	}
 
