487314df019b008455ba12524691f750c468ea9f
s9e <s9e.dev@gmail.com>
2013-07-08 00:09:40 +0200

    Fixing scripts

--- scripts/build/syncBranches.sh
+++ scripts/build/syncBranches.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+cd $(dirname $(realpath $0))
 git branch -D tmp 2> /dev/null
 
 git checkout -b tmp master
-$(dirname $(realpath $0))/convert5.4.php 5.4
+./convert5.4.php 5.4
 git commit -a --no-verify -m"Synced from master"
 git checkout dev/php5.4
 git merge -Xtheirs tmp
@@ -11,8 +12,8 @@ git merge -Xtheirs tmp
 git branch -D tmp
 
 git checkout -b tmp master
-$(dirname $(realpath $0))/convert5.4.php 5.4
-$(dirname $(realpath $0))/convert5.3.php 5.3
+./convert5.4.php 5.4
+./convert5.3.php 5.3
 git commit -a --no-verify -m"Synced from master"
 git checkout dev/php5.3
 git merge -Xtheirs tmp
