a78631ca5de4e381356b88d9ef8ebaaae47a3236
s9e <s9e.dev@gmail.com>
2013-07-08 00:31:52 +0200

    Fill the commit msg automatically

--- scripts/build/syncBranches.sh
+++ scripts/build/syncBranches.sh
@@ -3,20 +3,22 @@
 cd $(dirname $(realpath $0))
 git branch -D tmp 2> /dev/null
 
+msg="Synced from master"
+
 git checkout -b tmp master
 ./convert5.4.php 5.4
-git commit -a --no-verify -m"Synced from master"
+git commit -a --no-verify -m"$msg"
 git checkout dev/php5.4
-git merge -Xtheirs tmp
+git merge -Xtheirs -m"$msg" tmp
 
 git branch -D tmp
 
 git checkout -b tmp master
 ./convert5.4.php 5.4
 ./convert5.3.php 5.3
-git commit -a --no-verify -m"Synced from master"
+git commit -a --no-verify -m"$msg"
 git checkout dev/php5.3
-git merge -Xtheirs tmp
+git merge -Xtheirs -m"$msg" tmp
 
 git branch -D tmp
 git checkout master
