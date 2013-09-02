4e8b71f2b3a09f7e2e4a3c6a13f2c941e66837f0
s9e <s9e.dev@gmail.com>
2013-09-02 09:16:38 +0200

    Updated bundles

--- scripts/generateBundles.php
+++ scripts/generateBundles.php
@@ -11,6 +11,7 @@ foreach (glob(__DIR__ . '/../src/s9e/TextFormatter/Configurator/Bundles/*.php') 
 	$configurator = $className::getConfigurator();
 
 	$rendererGenerator = $configurator->setRendererGenerator('PHP');
+	$rendererGenerator->forceEmptyElements = false;
 	$rendererGenerator->className = 's9e\\TextFormatter\\Bundles\\' . $bundleName . '\\Renderer';
 	$rendererGenerator->filepath
 		= __DIR__ . '/../src/s9e/TextFormatter/Bundles/' . $bundleName . '/Renderer.php';
