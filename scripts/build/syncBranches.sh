5c7bf79182eed1d9b4762bd0d6a9b735686c2aae
s9e <s9e.dev@gmail.com>
2013-09-12 21:44:02 +0200

    Updated branch syncing script

--- scripts/build/syncBranches.sh
+++ scripts/build/syncBranches.sh
@@ -6,14 +6,15 @@ msg="Synced from master"
 
 for version in 5.4 5.3;
 do
-	git branch -D tmp/$version 2> /dev/null
-	git checkout -b tmp/$version master
+	branch="tmp/$version"
+	git branch -D $branch 2> /dev/null
+	git checkout -b $branch master
 	php patchSources.php $version
 	cp $version.composer.json ../../composer.json
 	cp $version.travis.yml ../../.travis.yml
 	git commit -a --no-verify -m"$msg"
 	git checkout dev/php$version
-	git merge -Xtheirs -m"$msg" tmp
+	git merge -Xtheirs -m"$msg" $branch
 done
 
 git checkout master
