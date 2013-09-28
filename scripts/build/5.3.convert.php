d2952a04d6528c808f1e7a00796451d89eb108cf
s9e <s9e.dev@gmail.com>
2013-09-28 09:45:54 +0200

    Fixed PHP 5.3 [ci skip]

--- scripts/build/5.3.convert.php
+++ scripts/build/5.3.convert.php
@@ -208,6 +208,12 @@ function convertCustom($filepath, &$file)
 				'call_user_func_array($callback, array(&$msg, &$context));'
 			)
 		),
+		'MarkdownLite/Parser.php' => array(
+			function ($str)
+			{
+				return str_replace('$decode(', 'call_user_func($decode, ', $str);
+			}
+		),
 		'PHP.php' => array(
 			array(
 				"protected \$dynamicParams=[' . implode(',', \$dynamicParams) . '];",
@@ -286,6 +292,12 @@ function convertCustom($filepath, &$file)
 		{
 			foreach ($pairs as $pair)
 			{
+				if (!is_array($pair))
+				{
+					$file = $pair($file);
+					continue;
+				}
+
 				list($search, $replace) = $pair;
 				$file = str_replace($search, $replace, $file);
 			}
