a87a45b1b57924a8414449c5e7a9c55148d697cf
s9e <s9e.dev@gmail.com>
2013-10-18 17:24:47 +0200

    Reaching code coverage Nirvana through Runkit and shenanigans

--- tests/Configurator/TemplateNormalizations/InlineInferredValuesTest.php
+++ tests/Configurator/TemplateNormalizations/InlineInferredValuesTest.php
@@ -2,11 +2,42 @@
 
 namespace s9e\TextFormatter\Tests\Configurator\TemplateNormalizations;
 
+use DOMDocument;
+use s9e\TextFormatter\Configurator\TemplateNormalizations\InlineInferredValues;
+
 /**
 * @covers s9e\TextFormatter\Configurator\TemplateNormalizations\InlineInferredValues
 */
 class InlineInferredValuesTest extends AbstractTest
 {
+	/**
+	* @testdox normalize() ignores unknown tokens from TemplateHelper::parseAttributeValueTemplate()
+	* @requires function runkit_method_redefine
+	* @runInSeparateProcess
+	*/
+	public function testUnknownToken()
+	{
+		class_exists('s9e\\TextFormatter\\Configurator\\Helpers\\TemplateHelper');
+
+		runkit_method_redefine(
+			's9e\\TextFormatter\\Configurator\\Helpers\\TemplateHelper',
+			'parseAttributeValueTemplate',
+			'',
+			"return [['foo']];",
+			RUNKIT_ACC_STATIC
+		);
+
+		$dom = new DOMDocument;
+		$dom->loadXML(
+			'<xsl:template xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+				<xsl:if test=".=\':)\'"><hr title="{.}"/></xsl:if>
+			</xsl:template>'
+		);
+
+		$normalizer = new InlineInferredValues;
+		$normalizer->normalize($dom->documentElement);
+	}
+
 	public function getData()
 	{
 		return [
