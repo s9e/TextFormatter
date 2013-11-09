071736f4137202fa176c1f3cfcd88bc8379b96c0
s9e <s9e.dev@gmail.com>
2013-11-09 10:36:33 +0100

    Updated build script [ci skip]

---
+++ scripts/build/syncReleaseBranches.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+git checkout master
+
+ignore=
+for file in $(ls -1A);
+do
+	if [[ "$file" != .git* && "$file" != "composer.json" && "$file" != "LICENSE" && "$file" != "README.md" && "$file" != "src" ]]
+	then
+		ignore="$ignore$file"$'\n'
+	fi
+done
+
+for version in 5.5 5.4 5.3
+do
+	branch="release/php$version"
+
+	if [ "$version" = "5.5" ]
+	then
+		src="master"
+	else
+		src="dev/php$version"
+	fi
+
+	git checkout "$branch"
+	echo "$ignore" > .gitignore
+	git merge -Xtheirs "$src" --no-edit
+	echo "$ignore" > .gitignore
+
+	for file in $ignore;
+	do
+		if [ -a "$file" ]
+		then
+			git rm -r --cached --quiet "$file" 2> /dev/null
+		fi
+	done
+
+	git commit -a --no-verify -m"Synced release branch from $src" --quiet
+done
+
+git checkout master
