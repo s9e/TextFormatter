c7e69bb7dc7fa74babaa3761c6522b8772e27421
s9e <s9e.dev@gmail.com>
2013-11-09 10:27:03 +0100

    Updated build script [ci skip]

--- scripts/build/syncBranches.sh
+++ scripts/build/syncBranches.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+cd $(dirname $(realpath $0))
+
 msg="Synced from master"
 
 for version in 5.4 5.3;
 do
-	cd $(dirname $(realpath $0))
 
 	branch="tmp-$version"
 	git branch -D $branch 2> /dev/null
@@ -15,14 +16,6 @@ do
 	git commit -a --no-verify -m"$msg"
 	git checkout dev/php$version
 	git merge -Xtheirs -m"$msg" $branch
-
-	cd $(dirname $(dirname $(dirname $(realpath $0))))
-
-	git checkout release/php$version
-	git merge -Xtheirs -m"$msg" dev/php$version
 done
-
-git checkout release/php5.5
-git merge -Xtheirs -m"$msg" master
 
 git checkout master
