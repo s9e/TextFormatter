3d1d1f13969537b2380c2b05d92dfb3862ba5155
s9e <s9e.dev@gmail.com>
2013-12-02 21:49:19 +0100

    Updated Travis setup [ci skip]

--- scripts/travis/setup.sh
+++ scripts/travis/setup.sh
@@ -5,15 +5,16 @@ cd $(dirname "$0")
 # Install Coveralls on 5.5 and disable XDebug on everything else
 if [ "$TRAVIS_PHP_VERSION" = "5.5" ]
 then
+	./installXSLCache.sh >/dev/null 2>&1 &
+
 	# We run this script detached in the background. It'll finish while tests are running
 	sh -c "./installComposer.sh 2>&1 &" >/dev/null 2>&1 &
 else
 	phpenv config-rm xdebug.ini
 fi
 
-# Install optional dependencies
+# Install Closure Compiler
 ./installClosureCompiler.sh >/dev/null 2>&1 &
-./installXSLCache.sh        >/dev/null 2>&1 &
 
 # The cache dir lets the MediaEmbed plugin cache scraped content
 mkdir ../../tests/.cache
