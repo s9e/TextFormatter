992bec84c17d412a9fe3e7d55eb2fb79ed6195c5
s9e <s9e.dev@gmail.com>
2013-12-13 15:26:03 +0100

    Updated maintenance script [ci skip]

--- scripts/patchExamples.php
+++ scripts/patchExamples.php
@@ -3,11 +3,6 @@
 
 include __DIR__ . '/../src/s9e/TextFormatter/autoloader.php';
 
-// Reuse the caching hack from the MediaEmbed tests
-eval('namespace s9e\TextFormatter\Tests;class Test{}');
-include __DIR__ . '/../tests/bootstrap.php';
-include __DIR__ . '/../tests/Plugins/MediaEmbed/ParserTest.php';
-
 function patchDir($dirpath)
 {
 	$dirpath = realpath($dirpath);
