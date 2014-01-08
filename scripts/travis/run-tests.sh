fa480e6be88f630f9a364b61a0dff0a81975b230
s9e <s9e.dev@gmail.com>
2014-01-08 21:01:38 +0100

    Fixed script

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -4,6 +4,7 @@ if [ "$TRAVIS_PHP_VERSION" = "5.5" ]
 then
 	phpunit --exclude-group none --coverage-clover /tmp/clover.xml
 elif [ "$TRAVIS_PHP_VERSION" = "hhvm" ]
+then
 	phpunit --exclude-group needs-network,no-hhvm
 else
 	phpunit --exclude-group needs-network
