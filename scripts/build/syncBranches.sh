da1e8010f038284abf0bb6a4ebe3976f0a60237a
s9e <s9e.dev@gmail.com>
2014-01-28 11:13:10 +0100

    Updated branch maintenance script [ci skip]

--- scripts/build/syncBranches.sh
+++ scripts/build/syncBranches.sh
@@ -5,30 +5,12 @@ cd $(dirname $(dirname $(dirname "$0")))
 git checkout master
 msg="Synced to $(git rev-parse HEAD)"
 
-ignore=
-for file in $(ls -1A);
-do
-	if [[ "$file" != .git* && "$file" != "composer.json" && "$file" != "LICENSE" && "$file" != "README.md" && "$file" != "src" && "$file" != ".travis.yml" ]]
-	then
-		ignore="$ignore$file"$'\n'
-	fi
-done
-
 for version in 5.5 5.4 5.3;
 do
 	tmp="tmp-$version"
 	rel="release/php$version"
 	git branch -D "$tmp" 2> /dev/null
 	git checkout -b "$tmp" master 2> /dev/null
-
-	for file in $ignore;
-	do
-		if [ -a "$file" ]
-		then
-			git rm -rq --cached "$file" 2> /dev/null
-		fi
-	done
-	echo "$ignore" > .gitignore
 
 	if [ -f "scripts/build/$version.composer.json" ]
 	then
@@ -38,9 +20,19 @@ do
 	php scripts/build/patchSources.php $version
 	php scripts/build/optimizeSources.php
 
-	git commit -aq --no-verify -m"$msg"
-	git show-branch "$rel" && git checkout "$rel" || git checkout -b "$rel" "$tmp"
-	git merge -Xtheirs -m"$msg" "$tmp"
+	git show-branch "$rel" || git branch "$rel"
+	patch=$(git diff --no-color "$rel")
+	git reset --hard
+
+	if [ -z "$patch" ]
+	then
+		echo "No patching: $rel is up-to-date"
+	else
+		git checkout "$rel"
+		echo "$patch" | git apply --whitespace=nowarn -
+		git commit -aq --no-verify -m"$msg"
+		git reset --hard master
+	fi
 done
 
 git checkout master
