cef48cc7a4ecead24a515cedd0f7709b67557104
s9e <s9e.dev@gmail.com>
2014-09-26 18:12:22 +0200

    MediaEmbed: updated Facebook

--- tests/Plugins/MediaEmbed/ParserTest.php
+++ tests/Plugins/MediaEmbed/ParserTest.php
@@ -1593,6 +1593,15 @@ class ParserTest extends Test
 				}
 			],
 			[
+				'https://www.facebook.com/events/787127511306384/permalink/849632838389184/',
+				'<r><FACEBOOK id="849632838389184" url="https://www.facebook.com/events/787127511306384/permalink/849632838389184/">https://www.facebook.com/events/787127511306384/permalink/849632838389184/</FACEBOOK></r>',
+				[],
+				function ($configurator)
+				{
+					$configurator->MediaEmbed->add('facebook');
+				}
+			],
+			[
 				'http://video.foxnews.com/v/3592758613001/reddit-helps-fund-homemade-hot-sauce-venture/',
 				'<r><FOXNEWS id="3592758613001" url="http://video.foxnews.com/v/3592758613001/reddit-helps-fund-homemade-hot-sauce-venture/">http://video.foxnews.com/v/3592758613001/reddit-helps-fund-homemade-hot-sauce-venture/</FOXNEWS></r>',
 				[],
