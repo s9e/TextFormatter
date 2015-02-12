3e43c4c6709fadefdae645fb1cc4733ad376da1e
s9e <s9e.dev@gmail.com>
2015-02-13 00:18:57 +0100

    Updated benchmark example

--- docs/examples/80_benchmark.php
+++ docs/examples/80_benchmark.php
@@ -10,9 +10,9 @@ first. Parsing and rendering plain text is a special case that uses code optimiz
 general, whether it's blogs or forums a majority of user content is plain text with no HTML, BBCodes
 or emoticons. Finally, we measure the time taken by render() and renderMulti() to render 10 texts.
 Running renderMulti() on an array of parsed texts should be more performant than rendering each text
-individually with render().
+individually with render(). (Updated: not anymore when the Quick renderer is involved)
 
-All the times are expressed in microseconds. (1/1,000,000th of a second)
+All times are expressed in microseconds. (1/1,000,000th of a second)
 
 */
 
@@ -20,7 +20,7 @@ include __DIR__ . '/../../src/autoloader.php';
 
 use s9e\TextFormatter\Bundles\Forum as TextFormatter;
 
-echo "All the times are expressed in microseconds. (1 millionth of a second)\n\n";
+echo "All times are expressed in microseconds. (1 millionth of a second)\n\n";
 
 $t1 = microtime(true);
 TextFormatter::getParser();
@@ -28,8 +28,8 @@ $t2 = microtime(true);
 TextFormatter::getRenderer();
 $t3 = microtime(true);
 
-printf("%5s µs - Loading parser\n", round(1000000 * ($t2 - $t1)));
-printf("%5s µs - Loading renderer\n", round(1000000 * ($t3 - $t2)));
+printf("%6s µs - Loading parser\n", round(1000000 * ($t2 - $t1)));
+printf("%6s µs - Loading renderer\n", round(1000000 * ($t3 - $t2)));
 
 $text = 'Hello, [i]world[/i] :)';
 $t1   = microtime(true);
@@ -38,8 +38,8 @@ $t2   = microtime(true);
 $html = TextFormatter::render($xml);
 $t3   = microtime(true);
 
-printf("%5s µs - Parsing rich text for the first time\n", round(1000000 * ($t2 - $t1)));
-printf("%5s µs - Rendering rich text for the first time\n", round(1000000 * ($t3 - $t2)));
+printf("%6s µs - Parsing rich text for the first time\n", round(1000000 * ($t2 - $t1)));
+printf("%6s µs - Rendering rich text for the first time\n", round(1000000 * ($t3 - $t2)));
 
 $text = 'Hello, [i]world[/i] :)';
 $t1   = microtime(true);
@@ -48,8 +48,8 @@ $t2   = microtime(true);
 $html = TextFormatter::render($xml);
 $t3   = microtime(true);
 
-printf("%5s µs - Parsing rich text for the second time\n", round(1000000 * ($t2 - $t1)));
-printf("%5s µs - Rendering rich text for the second time\n", round(1000000 * ($t3 - $t2)));
+printf("%6s µs - Parsing rich text for the second time\n", round(1000000 * ($t2 - $t1)));
+printf("%6s µs - Rendering rich text for the second time\n", round(1000000 * ($t3 - $t2)));
 
 $text = 'Hello, world!';
 $t1   = microtime(true);
@@ -58,8 +58,8 @@ $t2   = microtime(true);
 $html = TextFormatter::render($xml);
 $t3   = microtime(true);
 
-printf("%5s µs - Parsing plain text\n", round(1000000 * ($t2 - $t1)));
-printf("%5s µs - Rendering plain text\n", round(1000000 * ($t3 - $t2)));
+printf("%6s µs - Parsing plain text\n", round(1000000 * ($t2 - $t1)));
+printf("%6s µs - Rendering plain text\n", round(1000000 * ($t3 - $t2)));
 
 $text = str_repeat("A line of [b]rich[/b] text\n", 200);
 $t1   = microtime(true);
@@ -68,8 +68,18 @@ $t2   = microtime(true);
 $html = TextFormatter::render($xml);
 $t3   = microtime(true);
 
-printf("%5s µs - Parsing 200 lines of rich text\n", round(1000000 * ($t2 - $t1)));
-printf("%5s µs - Rendering 200 lines of rich text\n", round(1000000 * ($t3 - $t2)));
+printf("%6s µs - Parsing 200 lines of rich text\n", round(1000000 * ($t2 - $t1)));
+printf("%6s µs - Rendering 200 lines of rich text\n", round(1000000 * ($t3 - $t2)));
+
+$text = str_repeat(":) ", 1000);
+$t1   = microtime(true);
+$xml  = TextFormatter::parse($text);
+$t2   = microtime(true);
+$html = TextFormatter::render($xml);
+$t3   = microtime(true);
+
+printf("%6s µs - Parsing 1000 emoticons\n", round(1000000 * ($t2 - $t1)));
+printf("%6s µs - Rendering 1000 emoticons\n", round(1000000 * ($t3 - $t2)));
 
 $arr  = array();
 $text = 'Hello, [i]world[/i] :)';
@@ -87,6 +97,6 @@ $t3 = microtime(true);
 TextFormatter::renderMulti($arr);
 $t4 = microtime(true);
 
-printf("%5s µs - Parsing 10 rich texts\n", round(1000000 * ($t2 - $t1)));
-printf("%5s µs - Rendering 10 rich texts with render()\n", round(1000000 * ($t3 - $t2)));
-printf("%5s µs - Rendering 10 rich texts with renderMulti()\n", round(1000000 * ($t4 - $t3)));
+printf("%6s µs - Parsing 10 rich texts\n", round(1000000 * ($t2 - $t1)));
+printf("%6s µs - Rendering 10 rich texts with render()\n", round(1000000 * ($t3 - $t2)));
+printf("%6s µs - Rendering 10 rich texts with renderMulti()\n", round(1000000 * ($t4 - $t3)));
