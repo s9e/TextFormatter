6fa41277641bc204f111329069eb444001bf190c
s9e <s9e.dev@gmail.com>
2015-02-28 19:29:32 +0100

    Made the XSLCache renderer extend the XSLT renderer

--- src/Renderers/XSLCache.php
+++ src/Renderers/XSLCache.php
@@ -8,7 +8,6 @@
 namespace s9e\TextFormatter\Renderers;
 
 use XSLTCache;
-use s9e\TextFormatter\Renderer;
 
 /**
 * This renderer uses the xslcache PECL extension
@@ -16,22 +15,12 @@ use s9e\TextFormatter\Renderer;
 * @link http://pecl.php.net/package/xslcache
 * @link http://michaelsanford.com/compiling-xslcache-0-7-1-for-php-5-4/
 */
-class XSLCache extends Renderer
+class XSLCache extends XSLT
 {
 	/**
 	* @var string Path to the stylesheet used by this renderer
 	*/
 	protected $filepath;
-
-	/**
-	* @var XSLTCache The lazy-loaded XSLCache instance used by this renderer
-	*/
-	protected $proc;
-
-	/**
-	* @var bool Whether parameters need to be reloaded
-	*/
-	protected $reloadParams = false;
 
 	/**
 	* Constructor
@@ -42,52 +31,7 @@ class XSLCache extends Renderer
 	public function __construct($filepath)
 	{
 		$this->filepath = $filepath;
-
-		// Load the stylesheet for inspection
-		$stylesheet = file_get_contents($this->filepath);
-
-		// Capture the parameters' values from the stylesheet
-		preg_match_all('#<xsl:param name="([^"]+)"(?>/>|>([^<]+))#', $stylesheet, $matches);
-		foreach ($matches[1] as $k => $paramName)
-		{
-			$this->params[$paramName] = (isset($matches[2][$k]))
-			                          ? htmlspecialchars_decode($matches[2][$k])
-			                          : '';
-		}
-	}
-
-	/**
-	* Serializer
-	*
-	* @return string[] List of properties to serialize
-	*/
-	public function __sleep()
-	{
-		$props = get_object_vars($this);
-		unset($props['proc']);
-
-		if (empty($props['reloadParams']))
-		{
-			unset($props['reloadParams']);
-		}
-
-		return array_keys($props);
-	}
-
-	/**
-	* Unserialize helper
-	*
-	* Will reload parameters if they were changed between generation and serialization
-	*
-	* @return void
-	*/
-	public function __wakeup()
-	{
-		if (!empty($this->reloadParams))
-		{
-			$this->setParameters($this->params);
-			$this->reloadParams = false;
-		}
+		parent::__construct(file_get_contents($this->filepath));
 	}
 
 	/**
@@ -98,61 +42,6 @@ class XSLCache extends Renderer
 	public function getFilepath()
 	{
 		return $this->filepath;
-	}
-
-	/**
-	* {@inheritdoc}
-	*/
-	public function setParameter($paramName, $paramValue)
-	{
-		/**
-		* @link https://bugs.php.net/64137
-		*/
-		if (strpos($paramValue, '"') !== false
-		 && strpos($paramValue, "'") !== false)
-		{
-			$paramValue = str_replace('"', "\xEF\xBC\x82", $paramValue);
-		}
-		else
-		{
-			$paramValue = (string) $paramValue;
-		}
-
-		if (!isset($this->params[$paramName]) || $this->params[$paramName] !== $paramValue)
-		{
-			$this->load();
-			$this->proc->setParameter('', $paramName, $paramValue);
-			$this->params[$paramName] = $paramValue;
-			$this->reloadParams = true;
-		}
-	}
-
-	/**
-	* {@inheritdoc}
-	*/
-	protected function renderRichText($xml)
-	{
-		// Load the intermediate representation
-		$dom = $this->loadXML($xml);
-
-		// Load the stylesheet
-		$this->load();
-
-		// Perform the transformation and cast it as a string because it may return NULL if the
-		// transformation didn't output anything
-		$output = (string) $this->proc->transformToXml($dom);
-
-		// XSLTProcessor does not correctly identify <embed> as a void element. We fix it by
-		// removing </embed> end tags
-		$output = str_replace('</embed>', '', $output);
-
-		// Remove the \n that XSL adds at the end of the output, if applicable
-		if (substr($output, -1) === "\n")
-		{
-			$output = substr($output, 0, -1);
-		}
-
-		return $output;
 	}
 
 	/**
