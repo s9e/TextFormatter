41a39d1c0c441e14a68dcaad2fe90e2d49e9ca99
JoshyPHP <s9e.dev@gmail.com>
2015-03-21 12:14:33 +0100

    Added TransposeComments as a template normalization pass

---
+++ tests/Configurator/TemplateNormalizations/TransposeCommentsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace s9e\TextFormatter\Tests\Configurator\TemplateNormalizations;
+
+/**
+* @covers s9e\TextFormatter\Configurator\TemplateNormalizations\TransposeComments
+*/
+class TransposeCommentsTest extends AbstractTest
+{
+	public function getData()
+	{
+		return [
+			[
+				'<!-- This is a comment -->hi<!-- That one too -->',
+				'<xsl:comment> This is a comment </xsl:comment>hi<xsl:comment> That one too </xsl:comment>'
+			],
+			[
+				'<!-- A&B -->',
+				'<xsl:comment> A&amp;B </xsl:comment>'
+			],
+		];
+	}
+}
