cb9e9306e067b37d30a7ebcaeebac56678e65e76
JoshyPHP <s9e.dev@gmail.com>
2015-03-26 16:45:50 +0100

    Synced to 4f502becb79b179604639e8c69f6e3b462768820

--- src/Configurator/Items/AttributeFilters/SimpletextFilter.php
+++ src/Configurator/Items/AttributeFilters/SimpletextFilter.php
@@ -9,27 +9,17 @@ namespace s9e\TextFormatter\Configurator\Items\AttributeFilters;
 
 use s9e\TextFormatter\Configurator\Items\AttributeFilter;
 
-class Uint extends AttributeFilter
+class SimpletextFilter extends AttributeFilter
 {
 	public function __construct()
 	{
-		parent::__construct('s9e\\TextFormatter\\Parser\\BuiltInFilters::filterUint');
+		parent::__construct('s9e\\TextFormatter\\Parser\\BuiltInFilters::filterSimpletext');
 
 		$this->addParameterByName('attrValue');
-		$this->setJS('BuiltInFilters.filterUint');
+		$this->setJS('BuiltInFilters.filterSimpletext');
 	}
 
 	public function isSafeInCSS()
-	{
-		return \true;
-	}
-
-	public function isSafeInJS()
-	{
-		return \true;
-	}
-
-	public function isSafeAsURL()
 	{
 		return \true;
 	}
