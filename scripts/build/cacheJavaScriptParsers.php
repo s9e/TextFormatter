bf8aadfefb9d337c0129ed9ead79adcf62ea20c7
JoshyPHP <s9e.dev@gmail.com>
2015-04-16 07:16:43 +0200

    Added build script [ci skip]

---
+++ scripts/build/cacheJavaScriptParsers.php
@@ -0,0 +1,28 @@
+#!/usr/bin/php
+<?php
+
+include __DIR__ . '/../../src/autoloader.php';
+
+$configurator = new s9e\TextFormatter\Configurator;
+foreach (glob(__DIR__ . '/../../src/Plugins/*', GLOB_ONLYDIR) as $dirpath)
+{
+	$pluginName = basename($dirpath);
+	$js = $configurator->$pluginName->getJSParser();
+
+	$php = '
+	public function getJSParser()
+	{
+		return ' . var_export($js, true) . ';
+	}
+';
+
+	$filepath = $dirpath . '/Configurator.php';
+	$file = file_get_contents($filepath);
+	$pos = strrpos($file, '}');
+	$file = substr($file, 0, $pos) . $php . substr($file, $pos);
+	file_put_contents($filepath, $file);
+	unlink($dirpath . '/Parser.js');
+	echo "Removed $dirpath/Parser.js\n";
+}
+
+die("Done.\n");
