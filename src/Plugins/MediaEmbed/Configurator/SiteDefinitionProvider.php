cff90006bb0d18f124661aae220356443c996ed7
JoshyPHP <s9e.dev@gmail.com>
2015-04-17 05:45:31 +0200

    Synced to 08c86bdfd6211c4f42f8bcc92047c34679b383c7

--- src/Plugins/MediaEmbed/Configurator/SiteDefinitionProvider.php
+++ src/Plugins/MediaEmbed/Configurator/SiteDefinitionProvider.php
@@ -8,19 +8,31 @@
 namespace s9e\TextFormatter\Plugins\MediaEmbed\Configurator;
 
 use InvalidArgumentException;
+use RuntimeException;
 
 abstract class SiteDefinitionProvider
 {
 	public function get($siteId)
 	{
-		return $this->getSiteConfig($this->normalizeId($siteId));
+		$siteId = $this->normalizeId($siteId);
+		if (!$this->hasSiteConfig($siteId))
+			throw new RuntimeException("Unknown media site '" . $siteId . "'");
+
+		return $this->getSiteConfig($siteId);
 	}
 
 	abstract public function getIds();
 
+	public function has($siteId)
+	{
+		return $this->hasSiteConfig($this->normalizeId($siteId));
+	}
+
 	abstract protected function getSiteConfig($siteId);
 
-	public function normalizeId($siteId)
+	abstract protected function hasSiteConfig($siteId);
+
+	protected function normalizeId($siteId)
 	{
 		$siteId = \strtolower($siteId);
 		if (!\preg_match('(^[a-z0-9]+$)', $siteId))
