b06b02db55b7034d7f156aaa6aaf15b7c773c54b
JoshyPHP <s9e.dev@gmail.com>
2015-04-19 11:18:03 +0200

    Added test

--- tests/Configurator/RendererGenerators/PHP/SerializerTest.php
+++ tests/Configurator/RendererGenerators/PHP/SerializerTest.php
@@ -108,8 +108,34 @@ class SerializerTest extends Test
 							<output escape="text" type="literal">8</output>
 						</case>
 					</switch>
+					<switch branch-key="@bar">
+						<case branch-values=\'a:1:{i:0;s:1:"1";}\' test="@bar = 1">
+							<output escape="text" type="literal">1</output>
+						</case>
+						<case branch-values=\'a:1:{i:0;s:1:"2";}\' test="@bar = 2">
+							<output escape="text" type="literal">2</output>
+						</case>
+						<case branch-values=\'a:1:{i:0;s:1:"3";}\' test="@bar = 3">
+							<output escape="text" type="literal">3</output>
+						</case>
+						<case branch-values=\'a:1:{i:0;s:1:"4";}\' test="@bar = 4">
+							<output escape="text" type="literal">4</output>
+						</case>
+						<case branch-values=\'a:1:{i:0;s:1:"5";}\' test="@bar = 8">
+							<output escape="text" type="literal">5</output>
+						</case>
+						<case branch-values=\'a:1:{i:0;s:1:"6";}\' test="@bar = 6">
+							<output escape="text" type="literal">6</output>
+						</case>
+						<case branch-values=\'a:1:{i:0;s:1:"7";}\' test="@bar = 7">
+							<output escape="text" type="literal">7</output>
+						</case>
+						<case branch-values=\'a:2:{i:0;s:1:"8";i:1;s:2:"44";}\' test="@bar = 8 or @bar = 44">
+							<output escape="text" type="literal">8</output>
+						</case>
+					</switch>
 				</template>',
-				"if(isset(self::\$bt7794ED46[\$node->getAttribute('foo')])){\$n=self::\$bt7794ED46[\$node->getAttribute('foo')];if(\$n<4){if(\$n===0){\$this->out.='1';}elseif(\$n===1){\$this->out.='2';}elseif(\$n===2){\$this->out.='3';}else{\$this->out.='4';}}elseif(\$n===4){\$this->out.='5';}elseif(\$n===5){\$this->out.='6';}elseif(\$n===6){\$this->out.='7';}else{\$this->out.='8';}}",
+				"if(isset(self::\$bt7794ED46[\$node->getAttribute('foo')])){\$n=self::\$bt7794ED46[\$node->getAttribute('foo')];if(\$n<4){if(\$n===0){\$this->out.='1';}elseif(\$n===1){\$this->out.='2';}elseif(\$n===2){\$this->out.='3';}else{\$this->out.='4';}}elseif(\$n===4){\$this->out.='5';}elseif(\$n===5){\$this->out.='6';}elseif(\$n===6){\$this->out.='7';}else{\$this->out.='8';}}if(isset(self::\$bt7794ED46[\$node->getAttribute('bar')])){\$n=self::\$bt7794ED46[\$node->getAttribute('bar')];if(\$n<4){if(\$n===0){\$this->out.='1';}elseif(\$n===1){\$this->out.='2';}elseif(\$n===2){\$this->out.='3';}else{\$this->out.='4';}}elseif(\$n===4){\$this->out.='5';}elseif(\$n===5){\$this->out.='6';}elseif(\$n===6){\$this->out.='7';}else{\$this->out.='8';}}",
 				['bt7794ED46' => [1=>0,2=>1,3=>2,4=>3,5=>4,6=>5,7=>6,8=>7,44=>7]]
 			],
 		];
