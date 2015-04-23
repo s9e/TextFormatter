9e4779e4f336b97eddaacb8f4d58c07ae8e33ad5
JoshyPHP <s9e.dev@gmail.com>
2015-04-23 17:18:41 +0200

    Synced to 42d614c08e5f3ce5d35a29867d58a7f50fed7c91

--- src/Parser.php
+++ src/Parser.php
@@ -325,6 +325,26 @@ class Parser
 		return $isValid;
 	}
 
+	protected function encodeUnicodeSupplementaryCharacters()
+	{
+		$this->output = \preg_replace_callback(
+			'([\\xF0-\\xF4]...)',
+			__CLASS__ . '::encodeUnicodeSupplementaryCharactersCallback',
+			$this->output
+		);
+	}
+
+	protected function encodeUnicodeSupplementaryCharactersCallback($m)
+	{
+		$utf8 = $m[0];
+		$cp   = ((\ord($utf8[0]) & 7)  << 18)
+		      | ((\ord($utf8[1]) & 63) << 12)
+		      | ((\ord($utf8[2]) & 63) << 6)
+		      | (\ord($utf8[3]) & 63);
+
+		return '&#' . $cp . ';';
+	}
+
 	protected function finalizeOutput()
 	{
 		$this->outputText($this->textLen, 0, \true);
@@ -343,6 +363,8 @@ class Parser
 
 		if (\strpos($this->output, '</i><i>') !== \false)
 			$this->output = \str_replace('</i><i>', '', $this->output);
+
+		$this->encodeUnicodeSupplementaryCharacters();
 
 		$tagName = ($this->isRich) ? 'r' : 't';
 
