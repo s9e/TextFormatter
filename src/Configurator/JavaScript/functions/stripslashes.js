7fe80da8c9b6ef21f5951e9b91302263d46a6016
JoshyPHP <s9e.dev@gmail.com>
2015-06-12 21:58:47 +0200

    Synced to 4618abce7630ead96a5d685595267deb4fc0dca0

--- src/Configurator/JavaScript/functions/stripslashes.js
+++
@@ -1,6 +0,0 @@
-function(str)
-{
-	// NOTE: this will not correctly transform \0 into a NULL byte. I consider this a feature
-	//       rather than a bug. There's no reason to use NULL bytes in a text.
-	return str.replace(/\\([\s\S]?)/g, '\\1');
-}
