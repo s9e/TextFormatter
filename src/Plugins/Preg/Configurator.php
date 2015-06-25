5a6287769c8adf0f1868ff39252d02765fa4300a
JoshyPHP <s9e.dev@gmail.com>
2015-06-25 07:04:13 +0200

    Synced to a6cf876f85f40b3051ab9d25ecc9c3ce250371fe

--- src/Plugins/Preg/Configurator.php
+++ src/Plugins/Preg/Configurator.php
@@ -31,9 +31,9 @@ class Configurator extends ConfiguratorBase
 			return;
 		$pregs   = [];
 		$jsPregs = [];
-		foreach ($this->collection as $tagName => $_8a606a14)
+		foreach ($this->collection as $_ca164be8)
 		{
-			list($regexp, $passthroughIdx) = $_8a606a14;
+			list($tagName, $regexp, $passthroughIdx) = $_ca164be8;
 			$pregs[] = [$tagName, $regexp, $passthroughIdx];
 			if (isset($this->configurator->javascript))
 			{
@@ -47,6 +47,18 @@ class Configurator extends ConfiguratorBase
 			$variant->set('JS', $jsPregs);
 		return ['generics' => $variant];
 	}
+	public function match($regexp, $tagName)
+	{
+		$passthrough = 0;
+		$this->parseRegexp($regexp);
+		foreach ($this->captures as $i => $capture)
+		{
+			if (!$this->isCatchAll($capture['expr']))
+				continue;
+			$passthrough = $i;
+		}
+		$this->collection[] = [$tagName, $regexp, $passthrough];
+	}
 	public function replace($regexp, $template, $tagName = \null)
 	{
 		if (!isset($tagName))
@@ -58,7 +70,7 @@ class Configurator extends ConfiguratorBase
 			$this->captures[$passthrough]['passthrough'] = \true;
 		$regexp   = $this->fixUnnamedCaptures($regexp);
 		$template = $this->convertTemplate($template, $passthrough);
-		$this->collection[$tagName] = [$regexp, $passthrough];
+		$this->collection[] = [$tagName, $regexp, $passthrough];
 		return $this->createTag($tagName, $template);
 	}
 	protected function addAttribute(Tag $tag, $attrName)
@@ -157,7 +169,7 @@ class Configurator extends ConfiguratorBase
 		$passthrough = 0;
 		foreach ($this->references['inText'] as $key)
 		{
-			if (!\preg_match('(^\\.[*+]\\??$)D', $this->captures[$key]['expr']))
+			if (!$this->isCatchAll($this->captures[$key]['expr']))
 				continue;
 			if ($passthrough)
 			{
@@ -168,7 +180,7 @@ class Configurator extends ConfiguratorBase
 		}
 		return $passthrough;
 	}
-	protected function parseRegexp($regexp)
+	protected function getRegexpInfo($regexp)
 	{
 		$valid = \false;
 		try
@@ -180,8 +192,16 @@ class Configurator extends ConfiguratorBase
 			}
 		if ($valid === \false)
 			throw new InvalidArgumentException('Invalid regexp');
+		return RegexpParser::parse($regexp);
+	}
+	protected function isCatchAll($expr)
+	{
+		return (bool) \preg_match('(^\\.[*+]\\??$)D', $expr);
+	}
+	protected function parseRegexp($regexp)
+	{
 		$this->captures = [['name' => \null, 'expr' => \null]];
-		$regexpInfo = RegexpParser::parse($regexp);
+		$regexpInfo = $this->getRegexpInfo($regexp);
 		$this->delimiter = $regexpInfo['delimiter'];
 		$this->modifiers = \str_replace('D', '', $regexpInfo['modifiers']);
 		foreach ($regexpInfo['tokens'] as $token)
