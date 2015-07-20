dbc047e3f537d8e370be74e6228b8f23b693f3a5
JoshyPHP <s9e.dev@gmail.com>
2015-07-20 19:13:08 +0200

    Synced to 5d754d0776b1fbe45b3940eec9199e0074f2b193

--- src/Configurator.php
+++ src/Configurator.php
@@ -4006,6 +4006,43 @@ class DisallowXPathFunction extends TemplateCheck
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\TextFormatter\Configurator\TemplateNormalizations;
+use DOMAttr;
+use DOMElement;
+use DOMXPath;
+use s9e\TextFormatter\Configurator\TemplateNormalization;
+class FixUnescapedCurlyBracesInHtmlAttributes extends TemplateNormalization
+{
+	public function normalize(DOMElement $template)
+	{
+		$dom   = $template->ownerDocument;
+		$xpath = new DOMXPath($dom);
+		$query = '//@*[contains(., "{")]';
+		foreach ($xpath->query($query) as $attribute)
+			$this->fixAttribute($attribute);
+	}
+	protected function fixAttribute(DOMAttr $attribute)
+	{
+		$parentNode = $attribute->parentNode;
+		if ($parentNode->namespaceURI === self::XMLNS_XSL)
+			return;
+		$attribute->value = \htmlspecialchars(
+			\preg_replace(
+				'(\\b(?:do|else|(?:if|while)\\s*\\(.*?\\))\\s*\\{(?![{@]))',
+				'$0{',
+				$attribute->value
+			),
+			\ENT_NOQUOTES,
+			'UTF-8'
+		);
+	}
+}
+
+/*
+* @package   s9e\TextFormatter
+* @copyright Copyright (c) 2010-2015 The s9e Authors
+* @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace s9e\TextFormatter\Configurator\TemplateNormalizations;
 use DOMElement;
 use DOMException;
 use DOMText;
@@ -4666,6 +4703,7 @@ class TemplateNormalizer implements ArrayAccess, Iterator
 		$this->collection->append('PreserveSingleSpaces');
 		$this->collection->append('RemoveComments');
 		$this->collection->append('RemoveInterElementWhitespace');
+		$this->collection->append('FixUnescapedCurlyBracesInHtmlAttributes');
 		$this->collection->append('InlineAttributes');
 		$this->collection->append('InlineCDATA');
 		$this->collection->append('InlineElements');
