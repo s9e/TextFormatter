94c74315337fb618ed8371cfeada071522c64e77
JoshyPHP <s9e.dev@gmail.com>
2015-07-29 06:04:40 +0200

    Fixed docblock annotations in NullLogger.js

--- scripts/generateNullLogger.php
+++ scripts/generateNullLogger.php
@@ -4,12 +4,12 @@
 $dir = __DIR__ . '/../src/Parser';
 $js  = file_get_contents($dir . '/Logger.js');
 
-preg_match_all('/Logger\\.prototype\\.(\\w+) = (function\\([^)]*\\))/', $js, $matches, PREG_SET_ORDER);
+preg_match_all('((/\\*[^.]*?\\*/\\s*)Logger\\.prototype\\.(\\w+) = (function\\([^\\)]*\\)))', $js, $matches, PREG_SET_ORDER);
 
 $js = '/**@constructor*/function Logger(){}Logger.prototype={';
-foreach ($matches as list($line, $funcName, $signature))
+foreach ($matches as list($line, $doc, $funcName, $signature))
 {
-	$js .= $funcName . ':' . $signature . '{},';
+	$js .= $funcName . ':' . $doc . $signature . '{},';
 }
 $js = substr($js, 0, -1) . '}';
 
