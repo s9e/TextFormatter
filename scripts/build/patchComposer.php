79e5f5486f808fb020f457431829f4929e5d4ae0
JoshyPHP <s9e.dev@gmail.com>
2015-08-04 07:24:59 +0200

    Updated build script

--- scripts/build/patchComposer.php
+++ scripts/build/patchComposer.php
@@ -1,16 +1,14 @@
 #!/usr/bin/php
 <?php
 
+$match   = ['(,\\s*"scripts"[^}]+\\})'];
+$replace = [''];
+
+if (isset($_SERVER['argv'][1]))
+{
+	$match[]   = '(("php":\\s*"\\D*)[.\\d]++)';
+	$replace[] = '${1}' . $_SERVER['argv'][1];
+}
+
 $filepath = __DIR__ . '/../../composer.json';
-file_put_contents(
-	$filepath,
-	preg_replace(
-		'(,\\s*"scripts"[^}]+\\})',
-		'',
-		preg_replace(
-			'(("php":\\s*"\\D*)[.\\d]++)',
-			'${1}' . $_SERVER['argv'][1],
-			file_get_contents($filepath)
-		)
-	)
-);
+file_put_contents($filepath, preg_replace($match, $replace, file_get_contents($filepath)));
