0c0929b2c4c9a84158cbba342cb2bf7bf6f1ad52
JoshyPHP <s9e.dev@gmail.com>
2015-08-04 07:34:07 +0200

    Updated Travis script

--- scripts/travis/setup.sh
+++ scripts/travis/setup.sh
@@ -5,15 +5,15 @@ cd $(dirname "$0")
 echo "Disabling Zend GC"
 echo "zend.enable_gc=0" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
-# Install Coveralls if we're saving code coverage and disable XDebug otherwise
+# Install code coverage tools if applicable and disable XDebug otherwise
 if [ -n "$COVERAGE" ]
 then
 	echo "Installing Composer dependencies"
-	./installComposer.sh 2>&1 &
+	sh -c "./installComposer.sh 2>&1 &" >/dev/null 2>&1 &
 
 	# Install Scrutinizer's external code coverage tool
 	echo "Installing Scrutinizer"
-	./installScrutinizer.sh >/dev/null 2>&1 &
+	sh -c "./installScrutinizer.sh 2>&1 &" >/dev/null 2>&1 &
 else
 	echo "Removing XDebug"
 	phpenv config-rm xdebug.ini
