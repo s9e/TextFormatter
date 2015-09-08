4ee7865e9f0c4ff15ae5c31d57fcb797b3dfa9e6
JoshyPHP <s9e.dev@gmail.com>
2015-09-08 23:10:58 +0200

    Synced to 2ac29f1028e31392de343f80c65d1e3ae59b09bc

--- src/Plugins/MediaEmbed/Parser/tagFilter.js
+++ src/Plugins/MediaEmbed/Parser/tagFilter.js
@@ -1,9 +1,4 @@
 /**
-* Filter a MEDIA tag
-*
-* This will always invalidate the original tag, and possibly replace it with the tag that
-* corresponds to the media site
-*
 * @param  {!Tag}     tag   The original tag
 * @param  {!Object}  sites Map of [host => siteId]
 * @return {!boolean}       Always false
@@ -24,64 +19,37 @@ function (tag, sites)
 		return false;
 	}
 
-	var tagName;
-
-	if (tag.hasAttribute('media'))
+	/**
+	* Filter a MEDIA tag
+	*
+	* This will always invalidate the original tag, and possibly replace it with the tag that
+	* corresponds to the media site
+	*
+	* @param  {!Tag}     tag   The original tag
+	* @param  {!Object}  sites Map of [host => siteId]
+	* @return {!boolean}       Always false
+	*/
+	function filterTag(tag, sites)
 	{
-		// [media=youtube]xxxxxxx[/media]
-		var siteId = tag.getAttribute('media').toLowerCase();
-		if (!in_array(siteId, sites))
+		if (tag.hasAttribute('media'))
 		{
-			return false;
+			addTagFromMediaId(tag, sites);
 		}
-		tagName = siteId;
-
-		// If this tag doesn't have an id attribute and the url attribute doesn't really look
-		// like an URL, copy the value of the url attribute, so that the tag acts like
-		// [media=youtube id=xxxx]xxxx[/media]
-		if (!tag.hasAttribute('id')
-		 && tag.hasAttribute('url')
-		 && tag.getAttribute('url').indexOf('://') === -1)
+		else if (tag.hasAttribute('url'))
 		{
-			tag.setAttribute('id', tag.getAttribute('url'));
+			addTagFromMediaUrl(tag, sites);
 		}
-	}
-	else if (tag.hasAttribute('url'))
-	{
-		// Capture the scheme and host of the URL
-		var p = /^(?:([^:]+):)?(?:\/\/([^\/]+))?/.exec(tag.getAttribute('url'));
 
-		if (p[1] && sites[p[1] + ':'])
-		{
-			tagName = sites[p[1] + ':'];
-		}
-		else if (p[2])
-		{
-			var host = p[2];
-
-			// Start with the full host then pop domain labels off the start until we get a
-			// match
-			do
-			{
-				if (sites[host])
-				{
-					tagName = sites[host];
-					break;
-				}
-
-				var pos = host.indexOf('.');
-				if (pos == -1)
-				{
-					break;
-				}
-
-				host = host.substr(1 + pos);
-			}
-			while (host > '');
-		}
+		return false;
 	}
 
-	if (tagName)
+	/**
+	* Add a site tag
+	*
+	* @param {!Tag}    tag    The original tag
+	* @param {!string} siteId Site ID
+	*/
+	function addSiteTag(tag, siteId)
 	{
 		var endTag = tag.getEndTag() || tag;
 
@@ -90,10 +58,89 @@ function (tag, sites)
 			rpos = endTag.getPos() + endTag.getLen();
 
 		// Create a new tag and copy this tag's attributes and priority
-		var newTag = addSelfClosingTag(tagName.toUpperCase(), lpos, rpos - lpos);
+		var newTag = addSelfClosingTag(siteId.toUpperCase(), lpos, rpos - lpos);
 		newTag.setAttributes(tag.getAttributes());
 		newTag.setSortPriority(tag.getSortPriority());
 	}
 
-	return false;
+	/**
+	* Add a media site tag based on the attributes of a MEDIA tag
+	*
+	* @param {!Tag}    tag   The original tag
+	* @param {!Object} sites Map of [host => siteId]
+	*/
+	function addTagFromMediaId(tag, sites)
+	{
+		// If this tag doesn't have an id attribute and the url attribute doesn't really look
+		// like an URL, copy the value of the url attribute, so that the tag acts like
+		// [media=youtube id=xxxx]xxxx[/media]
+		if (!tag.hasAttribute('id') && tag.hasAttribute('url') && tag.getAttribute('url').indexOf('://') < 0)
+		{
+			tag.setAttribute('id', tag.getAttribute('url'));
+		}
+
+		// [media=youtube]xxxxxxx[/media]
+		var siteId = tag.getAttribute('media').toLowerCase();
+		if (in_array(siteId, sites))
+		{
+			addSiteTag(tag, siteId);
+		}
+	}
+
+	/**
+	* Add a media site tag based on the url attribute of a MEDIA tag
+	*
+	* @param {!Tag}    tag   The original tag
+	* @param {!Object} sites Map of [host => siteId]
+	*/
+	function addTagFromMediaUrl(tag, sites)
+	{
+		// Capture the scheme and (if applicable) host of the URL
+		var p = /^(?:([^:]+):)?(?:\/\/([^\/]+))?/.exec(tag.getAttribute('url')), siteId;
+		if (p[1] && sites[p[1] + ':'])
+		{
+			siteId = sites[p[1] + ':'];
+		}
+		else if (p[2])
+		{
+			siteId = findSiteIdByHost(p[2], sites);
+		}
+
+		if (siteId)
+		{
+			addSiteTag(tag, siteId);
+		}
+	}
+
+	/**
+	* Match a given host to a site ID
+	*
+	* @param  {!string}          host  Host
+	* @param  {!Object}          sites Map of [host => siteId]
+	* @return {!string|!boolean}       Site ID or FALSE
+	*/
+	function findSiteIdByHost(host, sites)
+	{
+		// Start with the full host then pop domain labels off the start until we get a match
+		do
+		{
+			if (sites[host])
+			{
+				return sites[host];
+			}
+
+			var pos = host.indexOf('.');
+			if (pos < 0)
+			{
+				break;
+			}
+
+			host = host.substr(1 + pos);
+		}
+		while (host > '');
+
+		return false;
+	}
+
+	return filterTag(tag, sites);
 }
