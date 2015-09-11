84ab0ed71e8b7110c6f3df376fe7a323859e7b96
JoshyPHP <s9e.dev@gmail.com>
2015-09-11 22:19:44 +0200

    Synced to 5ff9c1ec61df149a58ca9ca71b85a0fc87804d8a

---
+++ src/Plugins/MediaEmbed/Configurator/TemplateBuilder.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+* @package   s9e\TextFormatter
+* @copyright Copyright (c) 2010-2015 The s9e Authors
+* @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace s9e\TextFormatter\Plugins\MediaEmbed\Configurator;
+use s9e\TextFormatter\Plugins\MediaEmbed\Configurator\TemplateGenerators\Choose;
+use s9e\TextFormatter\Plugins\MediaEmbed\Configurator\TemplateGenerators\Flash;
+use s9e\TextFormatter\Plugins\MediaEmbed\Configurator\TemplateGenerators\Iframe;
+class TemplateBuilder
+{
+	public $responsiveEmbeds = \false;
+	protected $templateGenerators = [];
+	public function __construct()
+	{
+		$this->templateGenerators['choose'] = new Choose($this);
+		$this->templateGenerators['flash']  = new Flash;
+		$this->templateGenerators['iframe'] = new Iframe;
+	}
+	public function getTemplate(array $config)
+	{
+		foreach ($this->templateGenerators as $type => $generator)
+			if (isset($config[$type]))
+			{
+				$config[$type] += ['responsive' => $this->responsiveEmbeds];
+				return $generator->getTemplate($config[$type]);
+			}
+		return '';
+	}
+}
