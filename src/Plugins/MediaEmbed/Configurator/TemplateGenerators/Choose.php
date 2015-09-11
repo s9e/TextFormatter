e829f91c9555d452f47e31ef323d544257b3f98e
JoshyPHP <s9e.dev@gmail.com>
2015-09-11 22:19:43 +0200

    Synced to 5ff9c1ec61df149a58ca9ca71b85a0fc87804d8a

---
+++ src/Plugins/MediaEmbed/Configurator/TemplateGenerators/Choose.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+* @package   s9e\TextFormatter
+* @copyright Copyright (c) 2010-2015 The s9e Authors
+* @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace s9e\TextFormatter\Plugins\MediaEmbed\Configurator\TemplateGenerators;
+use s9e\TextFormatter\Plugins\MediaEmbed\Configurator\TemplateBuilder;
+use s9e\TextFormatter\Plugins\MediaEmbed\Configurator\TemplateGenerator;
+class Choose extends TemplateGenerator
+{
+	protected $templateBuilder;
+	public function __construct(TemplateBuilder $templateBuilder)
+	{
+		$this->templateBuilder = $templateBuilder;
+	}
+	public function getTemplate(array $attributes)
+	{
+		$branches = (isset($attributes['when'][0])) ? $attributes['when'] : [$attributes['when']];
+		$template = '<xsl:choose>';
+		foreach ($branches as $when)
+			$template .= '<xsl:when test="' . \htmlspecialchars($when['test'], \ENT_COMPAT, 'UTF-8') . '">' . $this->templateBuilder->getTemplate($when) . '</xsl:when>';
+		$template .= '<xsl:otherwise>' . $this->templateBuilder->getTemplate($attributes['otherwise']) . '</xsl:otherwise></xsl:choose>';
+		return $template;
+	}
+}
