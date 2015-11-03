97e363bec1c57e5023349832ece5f7376c5456e6
JoshyPHP <s9e.dev@gmail.com>
2015-11-03 18:46:54 +0100

    Synced to 845279a63baa882c506298afe5305c51c6b6ec08

--- src/Plugins/Escaper/Parser.js
+++ src/Plugins/Escaper/Parser.js
@@ -1,8 +1,10 @@
 matches.forEach(function(m)
 {
-	var tag = addVerbatim(m[0][1] + 1, m[0][0].length - 1);
-	tag.setFlags(0);
-	tag.setSortPriority(-1000);
-
-	addIgnoreTag(m[0][1], 1).cascadeInvalidationTo(tag);
+	addTagPair(
+		config.tagName,
+		m[0][1],
+		1,
+		m[0][1] + m[0][0].length,
+		0
+	);
 });
