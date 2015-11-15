dbc89066c125b76101972af88e7db745422faa7a
JoshyPHP <s9e.dev@gmail.com>
2015-11-15 21:02:17 +0100

    Synced to 54b40b81705cfdc2e1b3ccf016360428aabe03f5

--- src/Plugins/MediaEmbed/Configurator/TemplateGenerator.php
+++ src/Plugins/MediaEmbed/Configurator/TemplateGenerator.php
@@ -28,7 +28,7 @@ abstract class TemplateGenerator
 			$this->attributes['style']['position'] = 'absolute';
 			$this->attributes['style']['left'] = '0';
 			$outerStyle = 'display:inline-block;width:100%;max-width:' . $this->attributes['width'] . 'px';
-			$innerStyle = 'position:relative;' . $this->getResponsivePadding();
+			$innerStyle = 'overflow:hidden;position:relative;' . $this->getResponsivePadding();
 			$prepend .= '<div>' . $this->generateAttributes(['style' => $outerStyle]);
 			$prepend .= '<div>' . $this->generateAttributes(['style' => $innerStyle]);
 			$append  .= '</div></div>';
