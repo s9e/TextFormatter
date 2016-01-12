b042a4260fcdf778bac4538826c485bd955450d9
JoshyPHP <s9e.dev@gmail.com>
2016-01-12 09:25:17 +0100

    Updated Travis script

--- scripts/travis/installComposer.sh
+++ scripts/travis/installComposer.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-PACKAGES=matthiasmullie/minify
-if [ -n "$COVERAGE" ]
-then
-	PACKAGES="$PACKAGES satooshi/php-coveralls"
-fi
-
 cd $(dirname "$0")
 
 composer self-update
-composer require --no-interaction "$PACKAGES"
+composer require --no-interaction matthiasmullie/minify
+if [ -n "$COVERAGE" ]
+then
+	composer require --no-interaction satooshi/php-coveralls
+fi
 
 mv vendor ../..
