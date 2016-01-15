1050f78b5b2ebc4fe09a17b21d49d2d100a4f0ca
JoshyPHP <s9e.dev@gmail.com>
2016-01-15 14:45:26 +0100

    Updated #test

--- tests/Utils/Http/Clients/CurlTest.php
+++ tests/Utils/Http/Clients/CurlTest.php
@@ -6,8 +6,24 @@ use s9e\TextFormatter\Utils\Http\Clients\Curl;
 
 class CurlTest extends AbstractTest
 {
+	/**
+	* @beforeClass
+	*/
+	public static function removeCachedHandle()
+	{
+		HandleRemover::removeHandle();
+	}
+
 	protected function getInstance()
 	{
 		return new Curl;
+	}
+}
+
+class HandleRemover extends Curl
+{
+	public static function removeHandle()
+	{
+		self::$handle = null;
 	}
 }
