64f3ee0321bc6e38ecda7ccc8998a2e523ad6abd
JoshyPHP <s9e.dev@gmail.com>
2016-04-02 17:26:49 +0200

    Added OL and UL to the Forum bundle

--- src/Configurator/Bundles/Forum.php
+++ src/Configurator/Bundles/Forum.php
@@ -30,6 +30,7 @@ class Forum extends Bundle
 		$configurator->BBCodes->addFromRepository('LIST');
 		$configurator->BBCodes->addFromRepository('*');
 		$configurator->BBCodes->add('LI');
+		$configurator->BBCodes->addFromRepository('OL');
 		$configurator->BBCodes->addFromRepository('QUOTE', 'default', [
 			'authorStr' => '<xsl:value-of select="@author"/> <xsl:value-of select="$L_WROTE"/>'
 		]);
@@ -41,6 +42,7 @@ class Forum extends Bundle
 			'spoilerStr' => '{L_SPOILER}',
 		]);
 		$configurator->BBCodes->addFromRepository('U');
+		$configurator->BBCodes->addFromRepository('UL');
 		$configurator->BBCodes->addFromRepository('URL');
 
 		$configurator->rendering->parameters = [
