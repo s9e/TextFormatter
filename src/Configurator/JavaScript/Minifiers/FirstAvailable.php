ccc51abc86e59fbef64e551bbdae7804e08ebffe
JoshyPHP <s9e.dev@gmail.com>
2016-05-15 15:15:10 +0200

    Added phpdoc annotations #ignore

--- src/Configurator/JavaScript/Minifiers/FirstAvailable.php
+++ src/Configurator/JavaScript/Minifiers/FirstAvailable.php
@@ -14,6 +14,35 @@ use s9e\TextFormatter\Configurator\Collections\MinifierList;
 use s9e\TextFormatter\Configurator\JavaScript\Minifier;
 use s9e\TextFormatter\Configurator\Traits\CollectionProxy;
 
+
+/**
+* @method mixed   add(mixed $value, null $void)
+* @method mixed   append(mixed $value)
+* @method array   asConfig()
+* @method void    clear()
+* @method bool    contains(mixed $value)
+* @method integer count()
+* @method mixed   current()
+* @method void    delete(string $key)
+* @method bool    exists(string $key)
+* @method mixed   get(string $key)
+* @method mixed   indexOf(mixed $value)
+* @method mixed   insert(integer $offset, mixed $value)
+* @method integer|string key()
+* @method mixed   next()
+* @method integer normalizeKey(mixed $key)
+* @method Minifier normalizeValue(Minifier|string $minifier)
+* @method bool    offsetExists(string|integer $offset)
+* @method mixed   offsetGet(string|integer $offset)
+* @method void    offsetSet(mixed $offset, mixed $value)
+* @method void    offsetUnset(string|integer $offset)
+* @method string  onDuplicate(string|null $action)
+* @method mixed   prepend(mixed $value)
+* @method integer remove(mixed $value)
+* @method void    rewind()
+* @method mixed   set(string $key, mixed $value)
+* @method bool    valid()
+*/
 class FirstAvailable extends Minifier implements ArrayAccess
 {
 	use CollectionProxy;
