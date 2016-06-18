413fbeb3b2f5a7e4f48321fe377f3ff6562336e3
JoshyPHP <s9e.dev@gmail.com>
2016-06-18 14:50:58 +0200

    MediaEmbed: added support for Google Drive links that contain a domain name

--- tests/Plugins/MediaEmbed/ParserTest.php
+++ tests/Plugins/MediaEmbed/ParserTest.php
@@ -2004,6 +2004,15 @@ class ParserTest extends Test
 				}
 			],
 			[
+				'https://drive.google.com/a/monashores.net/file/d/0B-SjC2QWxqXRY2NfbGJ2QUcwTlU/view',
+				'<r><GOOGLEDRIVE id="0B-SjC2QWxqXRY2NfbGJ2QUcwTlU" url="https://drive.google.com/a/monashores.net/file/d/0B-SjC2QWxqXRY2NfbGJ2QUcwTlU/view">https://drive.google.com/a/monashores.net/file/d/0B-SjC2QWxqXRY2NfbGJ2QUcwTlU/view</GOOGLEDRIVE></r>',
+				[],
+				function ($configurator)
+				{
+					$configurator->MediaEmbed->add('googledrive');
+				}
+			],
+			[
 				'https://plus.google.com/110286587261352351537/posts/XMABm8rLvRW',
 				'<r><GOOGLEPLUS oid="110286587261352351537" pid="XMABm8rLvRW" url="https://plus.google.com/110286587261352351537/posts/XMABm8rLvRW">https://plus.google.com/110286587261352351537/posts/XMABm8rLvRW</GOOGLEPLUS></r>',
 				[],
