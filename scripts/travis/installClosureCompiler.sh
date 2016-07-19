f2061198a73895df732fbfe69bea801be3a81e84
JoshyPHP <s9e.dev@gmail.com>
2016-07-19 17:43:54 +0200

    Fixed Travis script

--- scripts/travis/installClosureCompiler.sh
+++ scripts/travis/installClosureCompiler.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget -O - http://dl.google.com/closure-compiler/compiler-latest.tar.gz | tar xzf - -C/tmp
+wget -O - http://dl.google.com/closure-compiler/compiler-latest.tar.gz | tar xzf - -C/tmp && \
+mv /tmp/closure-compiler-*.jar /tmp/compiler.jar
