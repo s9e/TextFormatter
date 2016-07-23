c01a1a101e69fca195c40308b98b863d7533a095
JoshyPHP <s9e.dev@gmail.com>
2016-07-23 19:08:56 +0200

    Synced to e307f649094178fa425afd2096db7f8a7590773b

--- src/Parser/BuiltInFilters.php
+++ src/Parser/BuiltInFilters.php
@@ -161,7 +161,7 @@ class BuiltInFilters
 	}
 	protected static function parseUrl($url)
 	{
-		$regexp = '(^(?:([a-z][-+.\\w]*):)?(?://(?:([^:/?#]*)(?::([^/?#]*)?)?@)?(?:(\\[[a-f\\d:]+\\]|[^:/?#]+)(?::(\\d*))?)?(?![^/?#]))?([^?#]*)(?:\\?([^#]*))?(?:#(.*))?$)Di';
+		$regexp = '(^(?:([a-z][-+.\\w]*):)?(?://(?:([^:/?#]*)(?::([^/?#]*)?)?@)?(?:(\\[[a-f\\d:]+\\]|[^:/?#]+)(?::(\\d*))?)?(?![^/?#]))?([^?#]*)(\\?[^#]*)?(#.*)?$)Di';
 		\preg_match($regexp, $url, $m);
 		$parts  = [];
 		$tokens = ['scheme', 'user', 'pass', 'host', 'port', 'path', 'query', 'fragment'];
@@ -197,11 +197,7 @@ class BuiltInFilters
 			if ($p['port'] !== '')
 				$url .= ':' . $p['port'];
 		}
-		$path = $p['path'];
-		if ($p['query'] !== '')
-			$path .= '?' . $p['query'];
-		if ($p['fragment'] !== '')
-			$path .= '#' . $p['fragment'];
+		$path = $p['path'] . $p['query'] . $p['fragment'];
 		$path = \preg_replace_callback(
 			'/%.?[a-f]/',
 			function ($m)
