f5a01b4b0bd8721538575fc6355a482a879d8c05
JoshyPHP <s9e.dev@gmail.com>
2016-07-29 04:03:52 +0200

    Synced to 636bd77147381ddb055de5d4a0ab42b0e50b45dc

--- src/Plugins/Litedown/Parser.js
+++ src/Plugins/Litedown/Parser.js
@@ -476,7 +476,7 @@ function matchBlockLevelMarkup()
 		}
 
 		// Close supernumerary quotes
-		if (quoteDepth < quotesCnt && !continuation && !lineIsEmpty)
+		if (quoteDepth < quotesCnt && !continuation)
 		{
 			newContext = true;
 
@@ -583,9 +583,9 @@ function matchBlockLevelMarkup()
 		{
 			var hasListItem = !!m[4];
 
-			if (!indentWidth && !continuation && !hasListItem && !lineIsEmpty)
+			if (!indentWidth && !continuation && !hasListItem)
 			{
-				// Start of a new paragraph
+				// Start of a new context
 				listIndex = -1;
 			}
 			else if (continuation && !hasListItem)
