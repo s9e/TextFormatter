c987d03f35f27a620845c00263928ad1a0572f16
JoshyPHP <s9e.dev@gmail.com>
2016-09-24 13:57:08 +0200

    Synced to 3c2ff8143055f7998d1cdab428157eaa67a44645

--- src/Plugins/Autovideo/Configurator.php
+++ src/Plugins/Autovideo/Configurator.php
@@ -5,21 +5,22 @@
 * @copyright Copyright (c) 2010-2016 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
-namespace s9e\TextFormatter\Plugins\Autoemail;
+namespace s9e\TextFormatter\Plugins\Autovideo;
+use s9e\TextFormatter\Configurator\Helpers\RegexpBuilder;
 use s9e\TextFormatter\Plugins\ConfiguratorBase;
 class Configurator extends ConfiguratorBase
 {
-	protected $attrName = 'email';
-	protected $quickMatch = '@';
-	protected $regexp = '/\\b[-a-z0-9_+.]+@[-a-z0-9.]*[a-z0-9]/Si';
-	protected $tagName = 'EMAIL';
+	protected $attrName = 'src';
+	protected $quickMatch = '://';
+	protected $regexp = '#\\bhttps?://[-.\\w]+/[-./\\w]+\\.(?:mp4|ogg|webm)(?!\\S)#i';
+	protected $tagName = 'VIDEO';
 	protected function setUp()
 	{
 		if (isset($this->configurator->tags[$this->tagName]))
 			return;
 		$tag = $this->configurator->tags->add($this->tagName);
-		$filter = $this->configurator->attributeFilters->get('#email');
+		$filter = $this->configurator->attributeFilters['#url'];
 		$tag->attributes->add($this->attrName)->filterChain->append($filter);
-		$tag->template = '<a href="mailto:{@' . $this->attrName . '}"><xsl:apply-templates/></a>';
+		$tag->template = '<video src="{@' . $this->attrName . '}"/>';
 	}
 }
