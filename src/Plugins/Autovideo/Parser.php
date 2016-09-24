7d233137736485b96eb353fde706ff045ec660d2
JoshyPHP <s9e.dev@gmail.com>
2016-09-24 13:57:07 +0200

    Synced to 3c2ff8143055f7998d1cdab428157eaa67a44645

--- src/Plugins/Autovideo/Parser.php
+++ src/Plugins/Autovideo/Parser.php
@@ -5,7 +5,7 @@
 * @copyright Copyright (c) 2010-2016 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
-namespace s9e\TextFormatter\Plugins\Autoimage;
+namespace s9e\TextFormatter\Plugins\Autovideo;
 use s9e\TextFormatter\Plugins\ParserBase;
 class Parser extends ParserBase
 {
