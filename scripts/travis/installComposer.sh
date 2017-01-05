c7c877a6427c5f5cee446493a2db6c3d11f576ad
JoshyPHP <s9e.dev@gmail.com>
2017-01-05 04:06:14 +0100

    Updated Travis script

--- scripts/travis/installComposer.sh
+++ scripts/travis/installComposer.sh
@@ -5,6 +5,6 @@ cd $(dirname $(dirname $(dirname "$0")))
 if [ -n "$COVERAGE" ]
 then
 	composer require --no-interaction satooshi/php-coveralls
-else
-	composer install --no-interaction
 fi
+
+composer install --no-interaction
