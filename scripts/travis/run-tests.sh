6cadf5febab7ec049457d38e44e6cccc1bd585ac
JoshyPHP <s9e.dev@gmail.com>
2017-01-14 17:48:46 +0100

    Updated Travis script [ci skip]

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -6,7 +6,7 @@ then
 	CACHE_PRELOAD=1 phpunit --group needs-network tests/Plugins/MediaEmbed/ParserTest.php > /dev/null &
 
 	# Wake up the remote minifier in case it's idling
-	curl -I http://s9e-textformatter.rhcloud.com/ 2>/dev/null >/dev/null &
+	curl -I http://s9e-textformatter.rhcloud.com/minifier/ 2>/dev/null >/dev/null &
 
 	phpunit --exclude-group needs-js --coverage-clover /tmp/clover.xml
 else
