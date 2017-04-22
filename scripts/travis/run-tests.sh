e0de18cb68298028d56fd1a34e0f2b2bd3645d23
JoshyPHP <s9e.dev@gmail.com>
2017-04-22 06:25:57 +0200

    Updated Travis script

--- scripts/travis/run-tests.sh
+++ scripts/travis/run-tests.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+cd $(dirname "$0")
+cd ../..
+
+CMD=phpunit
+if [ -f ./vendor/bin/phpunit ]
+then
+	CMD=./vendor/bin/phpunit
+fi
+
 if [ -n "$COVERAGE" ]
 then
 	# Run the network tests in parallel to populate the cache
-	CACHE_PRELOAD=1 phpunit --group needs-network tests/Plugins/MediaEmbed/ParserTest.php > /dev/null &
+	CACHE_PRELOAD=1 $CMD --group needs-network tests/Plugins/MediaEmbed/ParserTest.php > /dev/null &
 
 	# Wake up the remote minifier in case it's idling
 	curl -I http://s9e-textformatter.rhcloud.com/minifier/ 2>/dev/null >/dev/null &
 
-	phpunit --exclude-group needs-js --coverage-clover /tmp/clover.xml
+	$CMD --exclude-group needs-js --coverage-clover /tmp/clover.xml
 else
-	phpunit --exclude-group needs-network
+	$CMD --exclude-group needs-network
 fi
