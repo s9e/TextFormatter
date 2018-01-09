92251a805a84f8b0f60081bf0c7af3229eae7828
JoshyPHP <s9e.dev@gmail.com>
2018-01-09 08:43:35 +0100

    Updated Travis script

--- scripts/travis/installComposer.sh
+++ scripts/travis/installComposer.sh
@@ -3,13 +3,10 @@
 cd $(dirname "$0")
 cd ../..
 
-if [ "$TRAVIS_PHP_VERSION" = '5.3.3' ]
+if [ "$TRAVIS_PHP_VERSION" = '5.4' ]
 then
-	composer config disable-tls true
-	composer config secure-http false
-elif [[ "$TRAVIS_PHP_VERSION" != 5.* ]]
-then
-	composer require --no-interaction phpunit/phpunit:^5.7
+	composer remove --dev --no-interaction --ignore-platform-reqs s9e/regexp-builder
+	composer require --dev --no-interaction "php:^5.4"
 fi
 
 if [ -n "$COVERAGE" ]
@@ -17,4 +14,4 @@ then
 	composer require --no-interaction satooshi/php-coveralls
 fi
 
-composer install --no-interaction --ignore-platform-reqs
+composer install --no-interaction
