56f29f7619e3cf26aa4d63bd1d9723e9dcf59529
JoshyPHP <s9e.dev@gmail.com>
2018-05-20 18:12:25 +0200

    Synced to 22b9429ed9b208b96313ce35c410bd73995af599

--- src/Plugins/HTMLComments/Parser.php
+++ src/Plugins/HTMLComments/Parser.php
@@ -17,6 +17,7 @@ class Parser extends ParserBase
 		{
 			$content = \html_entity_decode(\substr($m[0][0], 4, -3), \ENT_QUOTES, 'UTF-8');
 			$content = \str_replace(['<', '>'], '', $content);
+			$content = \rtrim($content, '-');
 			$content = \str_replace('--', '', $content);
 			$this->parser->addSelfClosingTag($tagName, $m[0][1], \strlen($m[0][0]))->setAttribute($attrName, $content);
 		}
