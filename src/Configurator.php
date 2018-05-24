292d08fd794cb6e9fd988eb41cb4d29a63fe0a5b
JoshyPHP <s9e.dev@gmail.com>
2018-05-24 03:13:36 +0200

    Synced to cf10695198e3f9a05d9fd719f00493f103d9fded

--- src/Configurator.php
+++ src/Configurator.php
@@ -3451,10 +3451,11 @@ class Serializer
 	{
 		$php = '';
 		foreach ($ir->childNodes as $node)
-		{
-			$methodName = 'serialize' . \ucfirst($node->localName);
-			$php .= $this->$methodName($node);
-		}
+			if ($node instanceof DOMElement)
+			{
+				$methodName = 'serialize' . \ucfirst($node->localName);
+				$php .= $this->$methodName($node);
+			}
 		return $php;
 	}
 	protected function serializeCloseTag(DOMElement $closeTag)
