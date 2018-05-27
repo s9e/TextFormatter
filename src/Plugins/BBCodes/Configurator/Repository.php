48c2f8c40d80ed253f5a9fc2018ceb2c950bfce2
JoshyPHP <s9e.dev@gmail.com>
2018-05-27 16:59:56 +0200

    Synced to d973a468f048f05c4df6010b9bda4905556888d7

--- src/Plugins/BBCodes/Configurator/Repository.php
+++ src/Plugins/BBCodes/Configurator/Repository.php
@@ -11,48 +11,37 @@ use DOMElement;
 use DOMXPath;
 use InvalidArgumentException;
 use RuntimeException;
+use s9e\TextFormatter\Configurator\Items\Tag;
 class Repository
 {
 	protected $bbcodeMonkey;
 	protected $dom;
+	protected $xpath;
 	public function __construct($value, BBCodeMonkey $bbcodeMonkey)
 	{
 		$this->bbcodeMonkey = $bbcodeMonkey;
 		$this->dom          = ($value instanceof DOMDocument) ? $value : $this->loadRepository($value);
+		$this->xpath        = new DOMXPath($this->dom);
 	}
 	public function get($name, array $vars = [])
 	{
-		$name = \preg_replace_callback(
-			'/^[^#]+/',
-			function ($m)
-			{
-				return BBCode::normalizeName($m[0]);
-			},
-			$name
-		);
-		$xpath = new DOMXPath($this->dom);
-		$node  = $xpath->query('//bbcode[@name="' . \htmlspecialchars($name) . '"]')->item(0);
+		$name = BBCode::normalizeName($name);
+		$node = $this->xpath->query('//bbcode[@name="' . $name . '"]')->item(0);
 		if (!($node instanceof DOMElement))
 			throw new RuntimeException("Could not find '" . $name . "' in repository");
-		$clonedNode = $node->cloneNode(\true);
-		foreach ($xpath->query('.//var', $clonedNode) as $varNode)
-		{
-			$varName = $varNode->getAttribute('name');
-			if (isset($vars[$varName]))
-				$varNode->parentNode->replaceChild(
-					$this->dom->createTextNode($vars[$varName]),
-					$varNode
-				);
-		}
-		$usage      = $xpath->evaluate('string(usage)', $clonedNode);
-		$template   = $xpath->evaluate('string(template)', $clonedNode);
-		$config     = $this->bbcodeMonkey->create($usage, $template);
-		$bbcode     = $config['bbcode'];
-		$bbcodeName = $config['bbcodeName'];
-		$tag        = $config['tag'];
+		$node = $node->cloneNode(\true);
+		$this->replaceVars($node, $vars);
+		$usage    = $this->xpath->evaluate('string(usage)', $node);
+		$template = $this->xpath->evaluate('string(template)', $node);
+		$config   = $this->bbcodeMonkey->create($usage, $template);
 		if ($node->hasAttribute('tagName'))
-			$bbcode->tagName = $node->getAttribute('tagName');
-		foreach ($xpath->query('rules/*', $node) as $ruleNode)
+			$config['bbcode']->tagName = $node->getAttribute('tagName');
+		$this->addRules($node, $config['tag']);
+		return $config;
+	}
+	protected function addRules(DOMElement $node, Tag $tag)
+	{
+		foreach ($this->xpath->query('rules/*', $node) as $ruleNode)
 		{
 			$methodName = $ruleNode->nodeName;
 			$args       = [];
@@ -60,11 +49,6 @@ class Repository
 				$args[] = $ruleNode->textContent;
 			\call_user_func_array([$tag->rules, $methodName], $args);
 		}
-		return [
-			'bbcode'     => $bbcode,
-			'bbcodeName' => $bbcodeName,
-			'tag'        => $tag
-		];
 	}
 	protected function createRepositoryException($filepath)
 	{
@@ -79,5 +63,17 @@ class Repository
 		if (!$dom->loadXML(\file_get_contents($filepath), \LIBXML_NOERROR))
 			throw $this->createRepositoryException($filepath);
 		return $dom;
+	}
+	protected function replaceVars(DOMElement $node, array $vars)
+	{
+		foreach ($this->xpath->query('.//var', $node) as $varNode)
+		{
+			$varName = $varNode->getAttribute('name');
+			if (isset($vars[$varName]))
+				$varNode->parentNode->replaceChild(
+					$this->dom->createTextNode($vars[$varName]),
+					$varNode
+				);
+		}
 	}
 }
