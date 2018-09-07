0f3a11778a8a753d6d07e0004bb0db736ec6e6b2
JoshyPHP <s9e.dev@gmail.com>
2018-09-07 23:22:58 +0200

    Updated demo scripts [ci skip]

--- docs/examples/00_quick.php
+++ docs/examples/00_quick.php
@@ -19,5 +19,5 @@ echo $html, "\n";
 
 // Outputs:
 //
-// Hello, <i>world</i> <img alt=":)" class="emoji" draggable="false" src="//cdn.jsdelivr.net/emojione/assets/3.1/png/64/1f642.png"><br>
+// Hello, <i>world</i> <img alt=":)" class="emoji" draggable="false" src="//cdn.jsdelivr.net/emojione/assets/4.0/png/64/1f642.png"><br>
 // Find more examples in the <a href="https://s9etextformatter.readthedocs.io/">documentation</a>.
