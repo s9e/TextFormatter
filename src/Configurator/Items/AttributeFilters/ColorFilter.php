640eed514f6f5dc8ae6b73f6d8a71343e6a7f08e
JoshyPHP <s9e.dev@gmail.com>
2018-12-21 06:22:29 +0100

    ColorFilter: added support for alpha channel

--- src/Configurator/Items/AttributeFilters/ColorFilter.php
+++ src/Configurator/Items/AttributeFilters/ColorFilter.php
@@ -14,7 +14,7 @@ class ColorFilter extends RegexpFilter
 	*/
 	public function __construct()
 	{
-		parent::__construct('/^(?>#[0-9a-f]{3,6}|rgb\\(\\d{1,3}, *\\d{1,3}, *\\d{1,3}\\)|[a-z]+)$/Di');
+		parent::__construct('/^(?:#(?:(?:[0-9a-f]{3}){1,2}|(?:[0-9a-f]{4}){1,2})|rgb\\(\\d{1,3}, *\\d{1,3}, *\\d{1,3}\\)|rgba\\(\\d{1,3}, *\\d{1,3}, *\\d{1,3}, *\\d*(?:\\.\\d+)?\\)|[a-z]+)$/Di');
 		$this->markAsSafeInCSS();
 	}
 }
--- tests/Parser/AttributeFilters/ColorFilterTest.php
+++ tests/Parser/AttributeFilters/ColorFilterTest.php
@@ -13,9 +13,14 @@ class ColorFilterTest extends AbstractFilterTest
 	{
 		return [
 			[new ColorFilter, '#123abc', '#123abc'],
+			[new ColorFilter, '#123abcff', '#123abcff'],
+			[new ColorFilter, '#123', '#123'],
+			[new ColorFilter, '#123f', '#123f'],
 			[new ColorFilter, 'red', 'red'],
 			[new ColorFilter, 'rgb(12,34,56)', 'rgb(12,34,56)'],
 			[new ColorFilter, 'rgb(12, 34, 56)', 'rgb(12, 34, 56)'],
+			[new ColorFilter, 'rgba(12, 34, 56, 1)', 'rgba(12, 34, 56, 1)'],
+			[new ColorFilter, 'rgba(12, 34, 56, .5)', 'rgba(12, 34, 56, .5)'],
 			[new ColorFilter, '#1234567', false],
 			[new ColorFilter, 'blue()', false],
 		];
