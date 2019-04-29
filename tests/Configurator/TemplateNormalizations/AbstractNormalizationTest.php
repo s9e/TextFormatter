a1d91c252149e9bef4483323f15236f3e1e6ee1f
JoshyPHP <s9e.dev@gmail.com>
2019-04-29 17:06:11 +0200

    Added covering test

--- tests/Configurator/TemplateNormalizations/AbstractNormalizationTest.php
+++ tests/Configurator/TemplateNormalizations/AbstractNormalizationTest.php
@@ -126,7 +126,7 @@ class AbstractNormalizationTest extends Test
 
 		$this->assertXmlStringEqualsXmlString(
 			'<xsl:template xmlns:xsl="' . AbstractNormalization::XMLNS_XSL . '">
-				<div data-bar="BAR" data-foo="FOO"><hr/><xsl:comment/><b>...</b><xsl:text> </xsl:text><br/>Text</div>
+				<div data-bar="BAR" data-foo="FOO"><hr/><xsl:comment/><b>...</b><xsl:text> </xsl:text><br/> <br/>Text</div>
 				<span/><span/>
 			</xsl:template>',
 			$template->ownerDocument->saveXML()
@@ -184,6 +184,8 @@ class TestNormalization extends AbstractNormalization
 		$node->appendChild($this->createElement('xsl:comment'));
 		$node->appendChild($this->createElement('b', '...'));
 		$node->appendChild($this->createText(' '));
+		$node->appendChild($this->createElement('br'));
+		$node->appendChild($this->createTextNode(' '));
 		$node->appendChild($this->createElement('br'));
 		$node->appendChild($this->createText('Text'));
 	}
