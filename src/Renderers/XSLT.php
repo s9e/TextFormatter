571a82ce8cbc2cf10d8bcc6b7814d451e1a6fe12
JoshyPHP <s9e.dev@gmail.com>
2019-05-29 05:08:58 +0200

    Renderers\XSLT: removed unreachable code

--- src/Renderers/XSLT.php
+++ src/Renderers/XSLT.php
@@ -40,9 +40,7 @@ class XSLT extends Renderer
 		preg_match_all('#<xsl:param name="([^"]+)"(?>/>|>([^<]+))#', $stylesheet, $matches);
 		foreach ($matches[1] as $k => $paramName)
 		{
-			$this->params[$paramName] = (isset($matches[2][$k]))
-			                          ? htmlspecialchars_decode($matches[2][$k])
-			                          : '';
+			$this->params[$paramName] = htmlspecialchars_decode($matches[2][$k]);
 		}
 	}
 
