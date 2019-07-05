588435333a462dd206a044321cec43305ebb59ab
JoshyPHP <s9e.dev@gmail.com>
2019-07-05 16:10:49 +0200

    WiP

--- src/Plugins/BBCodes/Configurator/BBCodeDefinitionMatcher.php
+++ src/Plugins/BBCodes/Configurator/BBCodeDefinitionMatcher.php
@@ -17,28 +17,31 @@ class BBCodeDefinitionMatcher extends AbstractRecursiveMatcher
 	public function getMatchers(): array
 	{
 		return [
-			'Attribute'            => '((?&AttributeName))=((?&AttributeDefinition))',
-			'AttributeDefinition'  => '((?&Junk))((?&Token))((?&Junk))((?&AttributeDefinition)*)',
-			'AttributeName'        => '(\\w[-\\w]*)',
+			'Attribute'            => '((?&AttributeName))=((?&Tokens))',
+			'AttributeName'        => '\\w[-\\w]*',
 			'BBCodeAttribute'      => '((?&Attribute)|(?&Rule)|(?&TagOption))',
-			'BBCodeDefinition'     => '((?&StartTag))(?:((?&Content))((?&EndTag)))?',
+			'BBCodeDefinition'     => '((?&StartTag)) (?:((?&Content)) ((?&EndTag)))?',
 			'BBCodeName'           => '(\\*|\\w[-\\w]*)',
 			'CommaSeparatedValues' => '(\\w+(?:,\\w+)*)',
+			'Content'              => '((?&Tokens)?)',
 			'EndTag'               => '\\[/((?&BBCodeName))\\]',
 			'FilterValue'          => '((?&ArrayValue)|(?&UnquotedString))',
 			'Junk'                 => '(?:[^\\s\\{]|(?=\\{)(?!(?&Token))\\{)*+',
 			'Rule'                 => '#((?&RuleName))=((?&RuleValue))',
-			'RuleName'             => '(\\w+)',
+			'RuleName'             => '\\w+',
 			'RuleValue'            => '((?&False)|(?&True)|(?&CommaSeparatedValues))',
 			'StartTag'             => '\\[((?&BBCodeName)) ((?&Attributes)) (/?)\\]',
 			'TagOption'            => '((?&TagOptionName))=((?&TagOptionValue))',
-			'TagOptionName'        => '$([a-z]\\w*)',
+			'TagOptionName'        => '\\$[a-z]\\w*',
 			'TagOptionValue'       => '((?&ArrayValue)|(?&UnquotedString))',
 			'Token'                => '\\{((?&TokenName))(\\?)?(?:=((?&FilterValue)))?(?: ;((?&TokenOptions)))?\\}',
 			'TokenName'            => '[A-Z]+[0-9]*',
 			'TokenOption'          => '((?&TokenOptionName))(?:=((?&TokenOptionValue)))?',
+			'TokenOptionName'      => '\\w+',
+			'TokenOptionValue'     => '((?&ArrayValue)|(?&UnquotedString))',
 			'TokenOptions'         => '((?&TokenOption))(?:; ((?&TokenOptions)))?',
-			'UnquotedString'       => '([^\\s;\\]{}]*)'
+			'Tokens'               => '((?&Junk))((?&Token))((?&Junk))((?&Tokens))?',
+			'UnquotedString'       => '[^\\s;\\]{}]*'
 		];
 	}
 
