f5411a2b6f0063bf7c2230b958b71ea45f0fb10d
JoshyPHP <s9e.dev@gmail.com>
2019-10-10 19:36:17 +0200

    WiP

---
+++ src/Plugins/BBCodes/Configurator/BBCodeDefinitionParser.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+* @package   s9e\TextFormatter
+* @copyright Copyright (c) 2010-2019 The s9e Authors
+* @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace s9e\TextFormatter\Plugins\BBCodes\Configurator;
+
+// $filterChain.append="strtolower($attrValue)"
+// $filterChain.append("strtolower($attrValue)")
+// $attributePreprocessors.add("name", /regexp/)
+
+// foo={INT}
+// $attributes.add('foo')
+// $attributes.get('foo').filterChain.append('#int')
+class BBCodeDefinitionParser
+{
+	/**
+	* @var BBCodeDefinitionMatcher
+	*/
+	public $matcher;
+
+	/**
+	* @param  BBCodeDefinitionMatcher
+	* @return void
+	*/
+	public function __construct(BBCodeDefinitionMatcher $matcher = null): void
+	{
+		if (!isset($matcher))
+		{
+			$matcher = new BBCodeDefinitionMatcher;
+		}
+		$this->matcher = $matcher;
+	}
+}
