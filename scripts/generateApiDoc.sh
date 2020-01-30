384ff8853bb1984d2954a9b2a1bf13f22fd638f9
JoshyPHP <s9e.dev@gmail.com>
2020-01-30 17:17:16 +0100

    Updated APIdoc script [ci skip]

--- scripts/generateApiDoc.sh
+++ scripts/generateApiDoc.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-DIR="$(dirname $(dirname $(realpath $0)))/src"
+ROOT="$(dirname $(dirname $(realpath $0)))"
+DIR="$ROOT/src"
 TARGET="$(dirname $(dirname $(dirname $(realpath $0))))/s9e.github.io/TextFormatter/api"
 CONF="<?php return new Sami\\Sami('$DIR',['build_dir'=>'$TARGET','cache_dir'=>__DIR__.'/sami','store'=>new Sami\\Store\\ArrayStore]);"
 TMP_DIR="$(dirname $TARGET)/.cache"
 
-if [ ! -d "$TMP_DIR/sami" ]
-then
-	mkdir -p "$TMP_DIR/sami"
-fi
-
 cd "$TMP_DIR"
-if [ ! -f ./sami.phar ];
-then
-	wget https://get.sensiolabs.org/sami.phar
-fi
-
 echo "$CONF" > conf.php
-php sami.phar update conf.php -v
+php "$ROOT/vendor/bin/sami.php" update conf.php -v
 
 cd "$DIR"
 COMMIT_MSG="Updated API docs to $(git rev-parse HEAD)"
