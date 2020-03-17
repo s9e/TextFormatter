223641b0ef5ac36f5a8d87445b8c6b4e9d94506f
JoshyPHP <s9e.dev@gmail.com>
2020-03-18 00:04:59 +0100

    WiP

---
+++ src/Configurator/TemplateChecks/DisallowUnsupportedElements.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+* @package   s9e\TextFormatter
+* @copyright Copyright (c) 2010-2020 The s9e authors
+* @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace s9e\TextFormatter\Configurator\TemplateChecks;
+
+use DOMElement;
+use DOMXPath;
+use s9e\TextFormatter\Configurator\Exceptions\UnsafeTemplateException;
+use s9e\TextFormatter\Configurator\Items\Tag;
+use s9e\TextFormatter\Configurator\TemplateCheck;
+
+class DisallowUnsupportedElements extends DisallowNodeByXPath
+{
+	public function __construct()
+	{
+		$this->query = '';
+	}
+}
