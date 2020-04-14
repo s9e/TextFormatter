e41a832d829f379727e4b050aa23ba3bc44a1707
JoshyPHP <s9e.dev@gmail.com>
2020-04-14 23:20:44 +0200

    Updated script [ci skip]

--- scripts/regenerateMinifierCache.sh
+++ scripts/regenerateMinifierCache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname $(dirname $0))
 #rm -f tests/.cache/minifier.*
-ls -1r tests/Parser/AttributeFilters/*Test.php tests/Plugins/BBCodes/BBCodesTest.php tests/Plugins/*/ParserTest.php | xargs -n1 -P4 ./vendor/bin/phpunit --group needs-js 1>/dev/null 2>/dev/null
+ls -1r tests/Parser/AttributeFilters/*Test.php tests/Plugins/BBCodes/BBCodesTest.php tests/Plugins/*/ParserTest.php | xargs -tn1 -P4 ./vendor/bin/phpunit --group needs-js 1> /dev/null
