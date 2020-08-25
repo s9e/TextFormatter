009903884a00f48fadee58a8cffa8c1d58cf248a
JoshyPHP <s9e.dev@gmail.com>
2020-08-25 08:50:44 +0200

    Updated helper script [ci skip]

--- scripts/generateApiDoc.sh
+++ scripts/generateApiDoc.sh
@@ -2,12 +2,12 @@
 ROOT="$(dirname $(dirname $(realpath $0)))"
 DIR="$ROOT/src"
 TARGET="$(dirname $(dirname $(dirname $(realpath $0))))/s9e.github.io/TextFormatter/api"
-CONF="<?php return new Sami\\Sami('$DIR',['build_dir'=>'$TARGET','cache_dir'=>__DIR__.'/sami','store'=>new Sami\\Store\\ArrayStore]);"
+CONF="<?php return new Doctum\\Doctum('$DIR',['build_dir'=>'$TARGET','cache_dir'=>__DIR__.'/doctum','store'=>new Doctum\\Store\\ArrayStore]);"
 TMP_DIR="$(dirname $TARGET)/.cache"
 
 cd "$TMP_DIR"
 echo "$CONF" > conf.php
-php "$ROOT/vendor/bin/sami.php" update conf.php -v
+php "$ROOT/vendor/bin/doctum.php" update conf.php -v
 
 cd "$DIR"
 COMMIT_MSG="Updated API docs to $(git rev-parse HEAD)"
