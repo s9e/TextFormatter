ad48b8aae1a529d703008d83943c382991edf919
JoshyPHP <s9e.dev@gmail.com>
2021-02-15 18:56:10 +0100

    WiP

--- scripts/generateMediaEmbedTestPage.php
+++ scripts/generateMediaEmbedTestPage.php
@@ -11,23 +11,36 @@ if (isset($_SERVER['argv'][2]))
 	$configurator->rendering->parameters['MEDIAEMBED_THEME'] = $_SERVER['argv'][2];
 }
 
-$siteId = $_SERVER['argv'][1];
+$siteId     = $_SERVER['argv'][1];
+$siteConfig = $configurator->MediaEmbed->defaultSites[$siteId];
 $configurator->MediaEmbed->add($siteId);
+
+$isAmp = (isset($_SERVER['argv'][2]) && $_SERVER['argv'][2] === 'amp');
+$head  = '';
+if ($isAmp)
+{
+	$amp  = $siteConfig['amp'];
+	$head = '<script async src="https://cdn.ampproject.org/v0.js"></script>
+	<script custom-element="' . $amp['custom-element'] . '" src="' . $amp['src'] . '" async></script>';
+
+	$configurator->tags[$siteId]->template = $amp['template'];
+}
 
 extract($configurator->finalize());
 
 $html = "\n";
-foreach ($configurator->MediaEmbed->defaultSites[$siteId]['example'] as $example)
+foreach ($siteConfig['example'] as $example)
 {
 	$html .= $renderer->render($parser->parse($example)) . "\n";
 }
 
 $out = '<!DOCTYPE html>
-<html>
+<html' . ($isAmp ? "\u{26A1}" : '') . '>
 <head>
-	<meta charset="utf-8" />
+	<meta charset="utf-8">
 	<title>MediaEmbed test page</title>
 	<base href="http://localhost"/>
+	' . $head . '
 </head>
 <body>' . $html . '</body></html>';
 
