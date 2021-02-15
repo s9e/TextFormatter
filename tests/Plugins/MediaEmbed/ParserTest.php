c547af0585bb4c8e9d5e6584e040b680631b8145
JoshyPHP <s9e.dev@gmail.com>
2021-02-15 20:02:02 +0100

    WiP

--- tests/Plugins/MediaEmbed/ParserTest.php
+++ tests/Plugins/MediaEmbed/ParserTest.php
@@ -5439,34 +5439,44 @@ class ParserTest extends Test
 	{
 		return [
 			[
+				'dailymotion',
+				'https://www.dailymotion.com/video/x5e9eog?start=90',
+				'<amp-dailymotion layout="intrinsic" width="640" height="360" data-start="90" data-videoid="x5e9eog"></amp-dailymotion>'
+			],
+			[
 				'facebook',
 				'https://www.facebook.com/video/video.php?v=10150451523596807',
-				'<amp-facebook width="640" height="360" data-embed-as="video" data-href="https://www.facebook.com/user/videos/10150451523596807"></amp-facebook>'
+				'<amp-facebook layout="intrinsic" width="640" height="360" data-embed-as="video" data-href="https://www.facebook.com/user/videos/10150451523596807"></amp-facebook>'
 			],
 			[
 				'facebook',
 				'https://www.facebook.com/watch/?v=224353158889229',
-				'<amp-facebook width="640" height="360" data-embed-as="video" data-href="https://www.facebook.com/user/videos/224353158889229"></amp-facebook>'
+				'<amp-facebook layout="intrinsic" width="640" height="360" data-embed-as="video" data-href="https://www.facebook.com/user/videos/224353158889229"></amp-facebook>'
 			],
 			[
 				'facebook',
 				'https://www.facebook.com/FacebookDevelopers/posts/10151471074398553',
-				'<amp-facebook width="640" height="360" data-href="https://www.facebook.com/FacebookDevelopers/posts/10151471074398553"></amp-facebook>'
+				'<amp-facebook layout="intrinsic" width="640" height="360" data-href="https://www.facebook.com/FacebookDevelopers/posts/10151471074398553"></amp-facebook>'
+			],
+			[
+				'gfycat',
+				'https://gfycat.com/LoathsomeHarmfulJenny',
+				'<amp-gfycat layout="intrinsic" width="950" height="534" data-gfyid="LoathsomeHarmfulJenny"></amp-gfycat>'
 			],
 			[
 				'youtube',
 				'http://www.youtube.com/watch?v=wZZ7oFKsKzY',
-				'<amp-youtube width="640" height="360" data-param-list="" data-param-start="" data-videoid="wZZ7oFKsKzY"></amp-youtube>'
+				'<amp-youtube layout="intrinsic" width="640" height="360" data-param-list="" data-param-start="" data-videoid="wZZ7oFKsKzY"></amp-youtube>'
 			],
 			[
 				'youtube',
 				'http://www.youtube.com/watch?v=wZZ7oFKsKzY&t=23m45s',
-				'<amp-youtube width="640" height="360" data-param-list="" data-param-start="1425" data-videoid="wZZ7oFKsKzY"></amp-youtube>'
+				'<amp-youtube layout="intrinsic" width="640" height="360" data-param-list="" data-param-start="1425" data-videoid="wZZ7oFKsKzY"></amp-youtube>'
 			],
 			[
 				'youtube',
 				'http://www.youtube.com/watch?v=pC35x6iIPmo&list=PLOU2XLYxmsIIxJrlMIY5vYXAFcO5g83gA',
-				'<amp-youtube width="640" height="360" data-param-list="PLOU2XLYxmsIIxJrlMIY5vYXAFcO5g83gA" data-param-start="" data-videoid="pC35x6iIPmo"></amp-youtube>'
+				'<amp-youtube layout="intrinsic" width="640" height="360" data-param-list="PLOU2XLYxmsIIxJrlMIY5vYXAFcO5g83gA" data-param-start="" data-videoid="pC35x6iIPmo"></amp-youtube>'
 			],
 		];
 	}
