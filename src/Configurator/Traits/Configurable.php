66c107ed8d93883ed93223f57bb78ac6815d85c3
JoshyPHP <s9e.dev@gmail.com>
2022-07-12 04:12:52 +0200

    Updated file headers [ci skip]

--- src/Configurator/Traits/Configurable.php
+++ src/Configurator/Traits/Configurable.php
@@ -2,7 +2,7 @@
 
 /**
 * @package   s9e\TextFormatter
-* @copyright Copyright (c) 2010-2021 The s9e authors
+* @copyright Copyright (c) 2010-2022 The s9e authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\TextFormatter\Configurator\Traits;
